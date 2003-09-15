97fb5c9b1d8af333128c4cbcb04a333814244112
mg <>
2003-09-15 18:39:31 +0000

    Initial Twisted/ZODB integration.  This was tricky, because Twisted uses an event-driven approach, while ZODB relies on transparent blocking operations. In my solution traversal and rendering are performed in worker threads, while communications happen in Twisted's main loop.
    Several problems are not fully solved yet (ZODB ConflictErrors; error handling
    in general might need more testing).  Some tools will be needed for stress
    testing.  Functional testing frameworks should be modified to use a separate
    configuration file (different Data.fs etc.).

--- src/schooltool/main.py
+++ src/schooltool/main.py
@@ -24,9 +24,16 @@ Schooltool HTTP server.
 import os
 import sys
 import ZConfig
+import urllib
+import copy
+from persistence import Persistent
+from transaction import get_transaction
+from zope.pagetemplate.pagetemplatefile import PageTemplateFile
 from twisted.web import server, resource
 from twisted.internet import reactor
-from zope.pagetemplate.pagetemplatefile import PageTemplateFile
+from twisted.protocols import http
+from twisted.python import threadable
+from twisted.python import failure
 
 __metaclass__ = type
 
@@ -40,11 +47,13 @@ class FakePerson:
     def __init__(self, name):
         self.name = name
 
-class FakeApplication:
+class FakeApplication(Persistent):
 
     people = {'john': FakePerson('john'),
               'smith': FakePerson('smith'),
               'george': FakePerson('george')}
+
+    counter = 0
 
 
 #
@@ -79,6 +88,8 @@ class Template(PageTemplateFile):
 #
 # HTTP view infrastructure
 #
+
+SERVER_VERSION = "SchoolTool/0.1"
 
 class ErrorView(resource.Resource):
     """View for an error.
@@ -123,6 +134,11 @@ class View(resource.Resource):
     A View is a kind of a Resource in twisted.web sense, but it is really just
     a view for the actual resource, which is a content component.
 
+    Rendering and traversal happens in a separate worker thread.  It is
+    incorrect to call request.write or request.finish, or other non-thread-safe
+    methods.  You can read more in Twisted documentation section about
+    threading.
+
     Subclasses could provide the following methods and attributes:
 
         template    attribute that contains a Template instance for rendering
@@ -145,7 +161,7 @@ class View(resource.Resource):
         try:
             return self._traverse(name, request)
         except KeyError:
-            return NotFoundView(404, "Not found")
+            return NotFoundView(404, "Not Found")
         return self.__super_getChild(name, request)
 
     def _traverse(self, name, request):
@@ -154,8 +170,119 @@ class View(resource.Resource):
     def render(self, request):
         if request.method == 'GET':
             return self.template(request, view=self, context=self.context)
+        elif request.method == 'HEAD':
+            body = self.template(request, view=self, context=self.context)
+            request.setHeader('Content-Length', len(body))
+            return ""
         else:
-            return errorPage(request, 405, "Method not allowed")
+            request.setHeader('Allow', 'GET, HEAD')
+            return errorPage(request, 405, "Method Not Allowed")
+
+
+class Request(server.Request):
+    """Threaded request processor, integrated with ZODB"""
+
+    def process(self):
+        """Process the request"""
+
+        # Do all the things server.Request.process would do
+        self.site = self.channel.site
+        self.setHeader('Server', SERVER_VERSION)
+        self.setHeader('Date', http.datetimeToString())
+        self.setHeader('Content-Type', "text/html")
+        self.prepath = []
+        self.postpath = map(urllib.unquote, self.path[1:].split('/'))
+
+        # But perform traversal and rendering in a separate worker thread
+        reactor.callInThread(self._process)
+
+    def _process(self):
+        """Process the request in a separate thread.
+
+        Every request gets a separate transaction and a separate ZODB
+        connection.
+        """
+        self.zodb_conn = None
+        try:
+            try:
+                self.zodb_conn = self.site.db.open()
+                resrc = self.traverse()
+                self.render(resrc)
+                # XXX: if an exception happens after this point, note, that
+                #      request.write and request.finish might be already queued
+                #      with the reactor.
+                txn = get_transaction()
+                txn.note(self.path)
+                txn.setUser(self.getUser()) # anonymous is ""
+                txn.commit()
+            except:
+                get_transaction().abort()
+                reactor.callFromThread(self.processingFailed, failure.Failure())
+        finally:
+            if self.zodb_conn:
+                self.zodb_conn.close()
+                self.zodb_conn = None
+
+    def traverse(self):
+        """Locate the resource for this request.
+
+        This is called in a separate thread.
+        """
+
+        # Do things usually done by Site.getResourceFor
+        self.sitepath = copy.copy(self.prepath)
+        self.acqpath = copy.copy(self.prepath)
+
+        # Get a persistent application object from ZODB
+        root = self.zodb_conn.root()
+        app = root[self.site.rootName]
+        resource = self.site.viewFactory(app)
+        return resource.getChildForRequest(self)
+
+    def render(self, resrc):
+        """Render a resource.
+
+        This is called in a separate thread.
+        """
+        body = resrc.render(self)
+
+        if not isinstance(body, str):
+            body = errorPage(self, 500, "render did not return a string")
+
+        if self.method == "HEAD":
+            if len(body) > 0:
+                self.setHeader('Content-Length', len(body))
+            reactor.callFromThread(self.write, '')
+        else:
+            self.setHeader('Content-Length', len(body))
+            reactor.callFromThread(self.write, body)
+        reactor.callFromThread(self.finish)
+
+
+class Site(server.Site):
+    """Site for serving requests based on ZODB"""
+
+    __super = server.Site
+    __super___init__ = __super.__init__
+    __super_buildProtocol = __super.buildProtocol
+
+    def __init__(self, db, rootName, viewFactory):
+        """Creates a site.
+
+        Arguments:
+          db            ZODB database
+          rootName      name of the application object in the database
+          viewFactory   factory for the application object views
+        """
+        self.__super___init__(None)
+        self.db = db
+        self.viewFactory = viewFactory
+        self.rootName = rootName
+
+    def buildProtocol(self, addr):
+        channel = self.__super_buildProtocol(addr)
+        channel.requestFactory = Request
+        return channel
 
 
 #
@@ -166,6 +293,10 @@ class RootView(View):
     """View for the application root."""
 
     template = Template('www/root.pt')
+
+    def counter(self):
+        self.context.counter += 1
+        return self.context.counter
 
     def _traverse(self, name, request):
         if name == 'people':
@@ -204,14 +335,28 @@ class PersonView(View):
 
 def main():
     """Starts the SchoolTool mockup HTTP server on port 8080."""
+    threadable.init()
     dirname = os.path.dirname(__file__)
     schema = ZConfig.loadSchema(os.path.join(dirname, 'schema.xml'))
-    filename = os.path.join(dirname, '..', '..', 'schooltool.conf')
+
+    dirname = os.path.normpath(os.path.join(dirname, '..', '..'))
+    filename = os.path.join(dirname, 'schooltool.conf')
     if not os.path.exists(filename):
-        filename = os.path.join(dirname, '..', '..', 'schooltool.conf.in')
+        filename = os.path.join(dirname, 'schooltool.conf.in')
+    print "Reading configuration from %s" % filename
     config, handler = ZConfig.loadConfig(schema, filename)
 
-    site = server.Site(RootView(FakeApplication()))
+    reactor.suggestThreadPoolSize(config.thread_pool_size)
+
+    db = config.database.open()
+    conn = db.open()
+    root = conn.root()
+    if root.get('schooltool') is None:
+        root['schooltool'] = FakeApplication()
+        get_transaction().commit()
+    conn.close()
+
+    site = Site(db, 'schooltool', RootView)
     for interface, port in config.listen:
         reactor.listenTCP(port, site, interface=interface)
         print "Started HTTP server on %s:%s" % (interface or "*", port)
