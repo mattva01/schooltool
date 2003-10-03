405a42344500522ddc79dc5e60f557ec047a627a
steve <>
2003-10-03 15:17:51 +0000

    Added 'schema' directory for schemas, validation tools.
    Added a rough validation tool for relaxng schemas based on the validator
    from xmllib2.
    
    Added a first draft of a schema for an abstract group.

---
+++ src/schooltool/schema/validate_rng.py
@@ -0,0 +1,108 @@
+#!/usr/bin/env python2.3
+#
+# SchoolTool - common information systems platform for school administration
+# Copyright (c) 2003 Shuttleworth Foundation
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+#
+"""
+Check that a RelaxNG schema is valid, or validate an xml document against
+a RelaxNG schema.
+
+Usage: validate_rng schema [xmlfile]
+
+This program requires libxml2 and the python bindings for libxml2.
+libxml2 is available from http://www.xmlsoft.org/
+Python bindings are available from http://www.xmlsoft.org/python.html
+
+PoV are using libxml2 2.5.11-2 and libxml2-python2.3 2.5.11-2 from debian
+unstable. However, any recent release should work.
+
+"""
+import libxml2
+import sys
+
+def validateAgainstSchema(schema, xml):
+    rngp = libxml2.relaxNGNewMemParserCtxt(schema, len(schema))
+    try:
+        rngs = rngp.relaxNGParse()
+        ctxt = rngs.relaxNGNewValidCtxt()
+        ctxt.setErrorHandler(callback2, "-->")
+        doc = libxml2.parseDoc(xml)
+        try:
+            result = doc.relaxNGValidateDoc(ctxt)
+        finally:
+            doc.freeDoc()
+        return result == 0
+    finally:
+        # what does this do?
+        libxml2.relaxNGCleanupTypes()
+
+def loadSchema(schema):
+    rngp = libxml2.relaxNGNewMemParserCtxt(schema, len(schema))
+    try:
+        rngs = rngp.relaxNGParse()
+        ctxt = rngs.relaxNGNewValidCtxt()
+    finally:
+        # what does this do?
+        libxml2.relaxNGCleanupTypes()
+
+def callback1(ctx, str):
+    print "c1: %s:%s" % (ctx, str)
+
+def callback2(arg, msg, severity, reserved):
+    print "c2: %s:%s:%s:%s" % (arg, msg, severity, reserved)
+
+def printusage():
+    print >>sys.stderr, 'usage: validate_rng schema [xmlfile]'
+
+def main():
+    if len(sys.argv) == 1:
+        printusage()
+        return -1
+    argiter = iter(sys.argv)
+    pyfile = argiter.next()
+    xmlfile = None
+    try:
+        schemafile = argiter.next()
+        xmlfile = argiter.next()
+        shouldstophere = argiter.next()
+    except StopIteration:
+        pass
+    else:
+        printusage()
+        return -1
+
+    libxml2.registerErrorHandler(callback1, "-->")
+    schema = file(schemafile).read()
+
+    try:
+        if xmlfile:
+            xml = file(xmlfile).read()
+            validates_ok = validateAgainstSchema(schema, xml)
+            if not validates_ok:
+                print "Invalid"
+                return -1
+        else:
+            loadSchema(schema)
+    except libxml2.parserError, e:
+        print >>sys.stderr, e.msg
+        return -1
+    print "OK"
+    return 0
+
+if __name__ == '__main__':
+    sys.exit(main())
+
