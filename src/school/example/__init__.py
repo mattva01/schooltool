00eaee43592109e76c2b694142308f9cd3c3d7b1
alga <>
2003-11-03 19:45:12 +0000

    More prototype code for the example school.

--- src/school/example/__init__.py
+++ src/school/example/__init__.py
@@ -21,14 +21,16 @@ Package implementing add-ons for an example school.
 
 $Id: views.py 265 2003-10-29 15:36:42Z mg $
 """
-from zope.interface import moduleProvides, implements, classImplements
-from schooltool.interfaces import IModuleSetup, ISpecificURI, IFacet
-from schooltool.interfaces import IValencyConfigurable
-from schooltool.relationship import RelationshipSchema
-from schooltool.relationship import Valency
-from schooltool.facet import FacetFactory, FacetedRelationshipSchema
-from schooltool.component import registerFacetFactory
 
+from zope.interface import moduleProvides, implements, classImplements
+from schooltool.component import registerFacetFactory
+from schooltool.facet import FacetFactory, FacetedRelationshipSchema
+from schooltool.interfaces import IModuleSetup, ISpecificURI, IFacet
+from schooltool.interfaces import IRelationshipValencies
+from schooltool.membership import Membership
+from schooltool.relationship import RelationshipSchema
+from schooltool.relationship import RelationshipValenciesMixin
+from schooltool.relationship import Valency
 
 moduleProvides(IModuleSetup)
 
@@ -41,9 +43,11 @@ class URIParent(ISpecificURI):
 class URIChild(ISpecificURI):
     "http://schooltool.org/ns/example/parenthood/child"
 
-Parent = RelationshipSchema(URIParenthood, parent=URIParent, child=URIChild)
+Parenthood = RelationshipSchema(URIParenthood,
+                                parent=URIParent, child=URIChild)
 
-class FacetMixin:
+class FacetMixin(RelationshipValenciesMixin):
+
     implements(IFacet)
 
     __parent__ = None
@@ -61,18 +65,18 @@ class ParentFacet(FacetMixin):
         contactPhone = ''
         familialNature = ''
 
-# Decorating Parent above to be a faceted schema
-Parent = FacetedRelationshipSchema(
-            Parent, parent=FacetFactory(ParentFacet, 'Parent'))
+# Decorating Parenthood above to be a faceted schema
+Parenthood = FacetedRelationshipSchema(
+    Parenthood, parent=FacetFactory(ParentFacet, 'Parent'))
 
 class PupilFacet(FacetMixin):
 
+    valencies = Valency(Parenthood, 'child')
+
     def __init__(self):
-        fullName =''
+        fullName = ''
         dateOfBirth = None
         sex = None
-
-    # has Parenthood valency
 
 
 PupilMember = FacetedRelationshipSchema(
@@ -80,33 +84,95 @@ PupilMember = FacetedRelationshipSchema(
 
 class PupilGroupFacet(FacetMixin):
 
-    implements(IValencyConfigurable)
-
     valencies = Valency(PupilMember, 'group')
 
 
-# Teachers group
-# Teacher
+# Teachers group, Teacher
+
+class TeacherFacet(FacetMixin):
+    """A Teacher facet"""
+    pass
+
+TeachersGroupMembership = FacetedRelationshipSchema(
+    Membership, member=FacetFactory(TeacherFacet, 'Tutor'))
+
+class TeachersGroupFacet(FacetMixin):
+    """A facet for a group of teachers"""
+    valencies  = Valency(TeachersGroupMembership, 'group')
+
+
 # ClassTutor relationship
 # Class Tutor: receives events, and logs them for viewing.
+
+class URIClassTutor(ISpecificURI):
+    "http://schooltool.org/ns/example/classtutor"
+class URITutor(ISpecificURI):
+    "http://schooltool.org/ns/example/classtutor/tutor"
+class URIClass(ISpecificURI):
+    "http://schooltool.org/ns/example/classtutor/class"
+
+class TutorFacet(FacetMixin):
+    """A facet of a tutor of a class"""
+
+    eventTable = () # XXX
+
+ClassTutor = RelationshipSchema(URIClassTutor, cls=URIClass, tutor=URITutor)
+ClassTutor = FacetedRelationshipSchema(ClassTutor,
+                                       tutor=FacetFactory(TutorFacet, 'Tutor'))
+
 # school class:
 #   has tutor valency
+
+class SchoolClassFacet(FacetMixin):
+    """A facet of a class with a tutor"""
+
+    valencies = Valency(ClassTutor, 'cls')
+
 # tuition class:
 #   subject area
+
+class SubjectClassFacet(FacetMixin):
+    """A facet for a subject class"""
+    subject = None
+
 # year group:
 #   which year
+
+class YearGroupFacet(FacetMixin):
+    """A facet for a year group"""
+    year = None
+
 # departments
+
+class DepartmentFacet(FacetMixin):
+    title = None
+    pass
+
 # RegistrationClassesGroup:
 #   Sets RegistrationClass facet on Members.
 #   Has Tutor valency
 
+class RegistrationClassFacet(FacetMixin):
+    pass
 
-# views for
-#   PupilFacet
-#   ParentFacet
-#   ClassTutor
+RegistrationGroupMembership = FacetedRelationshipSchema(
+    Membership,
+    member=FacetFactory(RegistrationClassFacet, 'Registration Class'))
+
+class RegistrationClassGroupFacet(FacetMixin):
+    valencies = (Valency(RegistrationGroupMembership, 'group'),
+                 Valency(ClassTutor, 'cls'))
+
 
 def setUp():
     """Initialize the example add-ons."""
     print "Initializing example add-ons."
     registerFacetFactory(FacetFactory(PupilGroupFacet, 'Group of Pupils'))
+    registerFacetFactory(FacetFactory(SchoolClassFacet, 'Tuition Class'))
+    registerFacetFactory(FacetFactory(TeachersGroupFacet, 'Group of Teachers'))
+    registerFacetFactory(FacetFactory(RegistrationClassGroupFacet,
+                                      'Group of Registration Classes'))
+
+    import school.example.views
+    school.example.views.setUp()
+
