ad0f27b964d1b3505b39ccaae3e7c7282fb78aea
alga <>
2003-11-27 20:53:44 +0000

    A script for text export of calendars generated out of timetables.

---
+++ src/schooltool/textcal.py
@@ -0,0 +1,263 @@
+#!/usr/bin/env python2.3
+#
+# SchoolTool - common information systems platform for school administration
+# Copyright (c) 2003 Shuttleworth Foundation
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+#
+"""
+A text calendar script to demonstrate the calendaring functionality.
+
+$Id$
+"""
+import datetime
+import calendar
+from cal import Timetable, TimetableDay, TimetableActivity
+from cal import SchooldayModel, SchooldayPeriod, SchooldayTemplate
+from cal import SequentialDaysTimetableModel
+
+
+class Config:
+    """Hard-coded timetable configuration data for this script"""
+
+    days = ("1A", "2E", "3A", "4E")
+
+    timetable = { "1A" : { "P1": "Science",
+                           "P2": "Mathematics",
+                           "P3": "Beginning Spanish",
+                           "P4": "English"},
+                  "2E" : { "P1": "English",
+                           "P2": "Social studies",
+                           "P3": "Science",
+                           "P4": "Music"},
+                  "3A" : { "P1": "Science",
+                           "P2": "Mathematics",
+                           "P3": "Beginning Spanish",
+                           "P4": "Social studies"},
+                  "4E" : { "P1": "English",
+                           "P2": "Social studies",
+                           "P3": "Mathematics",
+                           "P4": "Music"}}
+
+    long_day = {"title": "L",
+                "P1": (datetime.time(9, 0), datetime.timedelta(hours=1)),
+                "P2": (datetime.time(10, 15), datetime.timedelta(hours=1)),
+                "P3": (datetime.time(11, 30), datetime.timedelta(hours=1)),
+                "P4": (datetime.time(12, 45), datetime.timedelta(hours=1))}
+
+    short_day = {"title": "S",
+                "P1": (datetime.time(9, 0), datetime.timedelta(hours=1)),
+                 "P2": (datetime.time(10, 5), datetime.timedelta(hours=1)),
+                 "P3": (datetime.time(11, 10), datetime.timedelta(hours=1)),
+                 "P4": (datetime.time(12, 15), datetime.timedelta(hours=1))}
+
+    day_templates = {"default": long_day,
+                     calendar.WEDNESDAY: short_day}
+
+    schooldays = {'first': datetime.date(2003, 9, 1),
+                  'last': datetime.date(2003, 12, 19),
+                  # Weekdays that are schooldays
+                  'weekdays': (calendar.MONDAY, calendar.TUESDAY,
+                               calendar.WEDNESDAY, calendar.THURSDAY,
+                               calendar.FRIDAY),
+                  # Exceptions -- holidays
+                  'holidays': (datetime.date(2003, 10, 13), # Columbus day
+                               datetime.date(2003, 11, 27), # Thanksgiving day
+                               datetime.date(2003, 11, 11), # Veteran's day
+                               )
+                  }
+
+
+def createSchooldayModel(config):
+    data = config.schooldays
+    sc = SchooldayModel(data['first'], data['last'])
+    sc.addWeekdays(*data['weekdays'])
+    for holiday in data['holidays']:
+        if sc.isSchoolday(holiday):
+            sc.remove(holiday)
+    return sc
+
+
+def createTimetable(config):
+    days = config.days
+    tt_data = config.timetable
+    tt = Timetable(days)
+    for day in tt_data:
+        ttday = TimetableDay(tt_data[day].keys())
+        tt[day] = ttday
+        for period in tt_data[day]:
+            ttday[period] = TimetableActivity(tt_data[day][period])
+    return tt
+
+
+def createTimetableModel(config):
+    dt_data = config.day_templates
+    dtemplates = {}
+    for weekday in dt_data:
+        dt = SchooldayTemplate()
+        title = dt_data[weekday]["title"]
+        dt.title = title
+        del dt_data[weekday]["title"]
+        for title, (start, duration) in dt_data[weekday].items():
+            dt.add(SchooldayPeriod(title, start, duration))
+        if weekday == "default":
+            weekday = None
+        dtemplates[weekday] = dt
+    return SequentialDaysTimetableModel(config.days, dtemplates)
+
+
+class PrintPacker:
+    """Basically, this class formats text printed with printLine()
+    method into n columns.
+    """
+
+    def __init__(self, cols=3, width=79):
+        self.cols = cols
+        self.width = width
+        self.columns = []
+        self.column = []
+
+    def printLine(self, line):
+        """Add a line to the current column"""
+        self.column.append(line)
+
+    def formFeed(self):
+        """Signals that a column is completed.
+
+        If enough columns are completed, they are printed.
+        """
+        self.columns.append(self.column)
+        self.column = []
+        if len(self.columns) == self.cols:
+            self.output()
+            print
+
+    def output(self):
+        """Print the data that is stored"""
+        if self.column:
+            self.columns.append(self.column)
+            self.column = []
+        if self.columns:
+            rows = reduce(max, [len(x) for x in self.columns])
+            self.columns += [] * (self.cols - len(self.columns))
+
+            for col in self.columns:
+                col += [''] * (rows - len(col))
+
+            for row in zip(*self.columns):
+                print "".join([s.ljust(self.width/self.cols) for s in row])
+        self.columns = []
+
+
+def printCalendar(cal, model):
+    gen = model._dayGenerator()
+    pp = PrintPacker(3, 79)
+    for date in cal.daterange:
+        daycal = cal.byDate(date)
+        events = list(daycal)
+        if events:
+            day = model.schooldayStrategy(date, gen)
+            s = "%s %s" % (day, date.strftime('%A %Y-%m-%d'))
+            pp.printLine(s)
+            pp.printLine("=" * len(s))
+            L = [(e.dtstart, e) for e in events]
+            L.sort()
+            for dt, event in L:
+                pp.printLine("%s %s" % (dt.strftime("%H:%M"), event.title))
+            pp.formFeed()
+    pp.output()
+
+
+def printCalendarSummary(cal, model):
+    gen = model._dayGenerator()
+    pp = PrintPacker(2)
+    week_nr = ['']
+    week_type = ['']
+    month = None
+    for date in cal.daterange:
+        daycal = cal.byDate(date)
+        events = list(daycal)
+        if events:
+            day = model.schooldayStrategy(date, gen).title()
+            length = model._getTemplateForDay(date).title
+        else:
+            day = "--"
+            length = "-"
+
+        if month is not None and month < date.month:
+            if date.weekday():
+                week_type += [' ' * 4] * (7 - date.weekday()) + ['']
+                week_nr += [' ' * 4] * (7 - date.weekday()) + ['']
+
+                pp.printLine("|".join(week_nr))
+                pp.printLine("|".join(week_type))
+                pp.printLine("+----+----+----+----+----+----+----+")
+            pp.formFeed()
+            week_type = [''] + [' ' * 4] * date.weekday()
+            week_nr = [''] + [' ' * 4] * date.weekday()
+
+        week_nr.append(date.strftime('%d').center(4))
+        week_type.append("%s %s" % (day, length))
+
+        if month is None or month < date.month:
+            m = date.strftime("%B %Y")
+            pp.printLine(m.center(35))
+            # pp.printLine(("-" * len(m)).center(35))
+            pp.printLine("+----+----+----+----+----+----+----+")
+            pp.printLine("|Mon |Tue |Wed |Thu |Fri |Sat |Sun |")
+            pp.printLine("+----+----+----+----+----+----+----+")
+            month = date.month
+
+        if date.weekday() == 6:
+            week_nr.append("")
+            week_type.append("")
+            pp.printLine("|".join(week_nr))
+            pp.printLine("|".join(week_type))
+            pp.printLine("+----+----+----+----+----+----+----+")
+            week_type = ['']
+            week_nr = ['']
+    if week_nr:
+        if date.weekday():
+            week_type += [' ' * 4] * (6 - date.weekday()) + ['']
+            week_nr += [' ' * 4] * (6 - date.weekday()) + ['']
+            pp.printLine("|".join(week_nr))
+            pp.printLine("|".join(week_type))
+            pp.printLine("+----+----+----+----+----+----+----+")
+        pp.output()
+
+
+def main():
+    scoolday_model = createSchooldayModel(Config)
+    timetable_model = createTimetableModel(Config)
+    timetable = createTimetable(Config)
+
+    cal = timetable_model.createCalendar(scoolday_model, timetable)
+
+    print "============="
+    print "FULL CALENDAR"
+    print "============="
+
+    printCalendar(cal, timetable_model)
+
+    print
+    print "==============="
+    print "SCHEMATIC MODEL"
+    print "==============="
+
+    printCalendarSummary(cal, timetable_model)
+
+
+if __name__ == '__main__':
+    main()
