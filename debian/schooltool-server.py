c6ecd5764f49738681002dc36d7bd7f5e2208ec8
mg <>
2004-04-19 17:35:31 +0000

    Split the server and the client parts into separate Debian packages.

--- debian/schooltool-server.py
+++ debian/schooltool-server.py
@@ -4,7 +4,7 @@ A script to start the schooltool server in a Debian system.
 """
 
 import sys
-sys.path.insert(0, '/usr/lib/schooltool')
+sys.path.insert(0, '/usr/lib/schooltool-server')
 
 # Change the default config file name by prepending a command-line argument
 sys.argv.insert(1, '--config=/etc/schooltool.conf')
