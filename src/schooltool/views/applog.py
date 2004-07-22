0d88997f03b8f4d6d10fd751b49c5ac808420e5f
gintas <>
2004-07-22 08:17:58 +0000

    Renamed the variable 'filter' to 'filter_str' (I dislike shadowing of builtins).

--- src/schooltool/views/applog.py
+++ src/schooltool/views/applog.py
@@ -41,9 +41,9 @@ class ApplicationLogView(View):
             file = self.openLog(path)
             try:
                 if 'filter' in request.args:
-                    filter = request.args['filter']
+                    filter_str = request.args['filter']
                     result = [line for line in file.readlines()
-                              if filter in line]
+                              if filter_str in line]
                     # set the size of result in some header
                     if 'page' in request.args and 'pagesize' in request.args:
                         page = int(request.args['page'])
