95710f501e1669123969adcf53f1f9843c0ba195
jinty <>
2004-09-19 16:43:21 +0000

    Created libschooltool-server package for arch dependent server files

--- debian/schooltool-server.py
+++ debian/schooltool-server.py
@@ -4,7 +4,7 @@ A script to start the schooltool server in a Debian system.
 """
 
 import sys
-sys.path.insert(0, '/usr/lib/schooltool-server')
+sys.path.insert(0, '/usr/share/schooltool-server')
 
 # Change the default config file name by prepending a command-line argument
 sys.argv.insert(1, '--config=/etc/schooltool.conf')
