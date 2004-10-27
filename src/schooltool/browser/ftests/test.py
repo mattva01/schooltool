b610e744825288afdd9ec2b6e9bae8f9f174c053
mg <>
2004-10-27 15:06:13 +0000

    Moved browser functional test helpers to schooltool.browser.ftests.

--- src/schooltool/browser/ftests/test.py
+++ src/schooltool/browser/ftests/test.py
@@ -24,7 +24,92 @@ import unittest
 import urllib
 
 from schooltool.ftests import setup
-from schooltool.browser.ftests import Browser
+
+
+#
+# Helper classes and functions
+#
+
+class URLOpener(urllib.URLopener):
+    """Customization of urllib.URLopener.
+
+    Adds two attributes (`status` and `message`) to file objects returned
+    when you open HTTP or HTTPS URLs.
+    """
+
+    def http_error(self, url, fp, errcode, errmsg, headers, data=None):
+        """Default error handling -- don't raise an exception."""
+        fp = urllib.addinfourl(fp, headers, "http:" + url)
+        fp.status = errcode
+        fp.message = errmsg
+        return fp
+
+    def open_http(self, url, data=None):
+        fp = urllib.URLopener.open_http(self, url, data)
+        if not hasattr(fp, 'status'):
+            fp.status = 200
+            fp.message = 'OK'
+        return fp
+
+    def open_https(self, url, data=None):
+        fp = urllib.URLopener.open_https(self, url, data)
+        if not hasattr(fp, 'status'):
+            fp.status = 200
+            fp.message = 'OK'
+        return fp
+
+
+class Browser(object):
+    """Class emulating a web browser.
+
+    The emulation is not very complete.  HTTP redirects (only code 302)
+    are processed.  There is some very crude basic support for cookies.
+
+    Attributes:
+
+        url         URL of the current page.
+        content     content of the current page.
+        status      HTTP status code.
+        message     HTTP status message.
+        headers     a mimelib.Message containing HTTP response headers.
+
+    """
+
+    def __init__(self):
+        self.url = None
+        self.content = None
+        self._cookies = {}
+
+    def go(self, url):
+        """Go to a specified URL."""
+        self._open(url)
+
+    def post(self, url, form):
+        """Post a form to a specified URL."""
+        self._open(url, urllib.urlencode(form))
+
+    def _open(self, url, data=None):
+        self.url = url
+        opener = URLOpener()
+        for name, value in self._cookies.items():
+            # Quick and dirty hack
+            opener.addheader('Cookie', '%s=%s' % (name, value))
+        fp = opener.open(url, data=data)
+        self.content = fp.read()
+        self.headers = fp.info()
+        self.status = fp.status
+        self.message = fp.message
+        fp.close()
+        for cookie in self.headers.getheaders('Set-Cookie'):
+            # Quick and dirty hack
+            args = cookie.split(';')
+            name, value = args[0].split('=')
+            self._cookies[name] = value
+        if self.status == 302:
+            # Process HTTP redirects.
+            location = self.headers.get('Location')
+            # This might loop.  Let's hope it doesn't
+            self._open(location)
 
 
 #
