1699b07fab505ff09ff24f14221cdb012c98d6ff
jinty <>
2004-11-03 16:23:50 +0000

    * Add release notes to packages.   * Merge build the schoolbell package into the schooltool source package.     The schoolbell source package will be removed from debian.

---
+++ debian/schoolbell.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python2.3
+"""
+A script to start the SchoolBell server in a Debian system.
+"""
+
+import sys
+sys.path.insert(0, '/usr/share/schoolbell')
+sys.path.insert(0, '/usr/lib/schooltool')
+
+# Change the default config file name by prepending a command-line argument
+sys.argv.insert(1, '--config=/etc/schoolbell/schoolbell.conf')
+
+import schooltool.main
+schooltool.main.main()
