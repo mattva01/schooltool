09f56a07ac89aa37e5b5df913a04399dfbacff3c
mg <>
2005-01-19 12:55:52 +0000

    Make schoolbell.app functional tests run both with SchoolTool's and with Zope's test runners.

--- src/schoolbell/app/browser/ftests.py
+++ src/schoolbell/app/browser/ftests.py
@@ -2,12 +2,24 @@
 Functional tests for schoolbell.app.app.
 """
 
+import os
 import unittest
 
+from zope.app.tests.functional import FunctionalTestSetup
 from zope.app.tests.functional import FunctionalDocFileSuite
 
 
 def test_suite():
+    # Hack to make the test work with SchoolTool's test runner:
+    # tell zope.app.tests.functional that ftesting.zcml resides
+    # in the Zope3 subdirectory
+    try:
+        FunctionalTestSetup(os.path.join('Zope3', 'ftesting.zcml'))
+    except NotImplementedError:
+        pass # FunctionalTestSetup raises NotImplementedError when called twice
+             # with a different config_file argument.  For us this indicates
+             # that Zope 3's test runner already configured the functional
+             # testing machinery with the correct ftesting.zcml.
     return unittest.TestSuite([
                 FunctionalDocFileSuite('ftest.txt'),
            ])
