be31f7639318828667d4945c0fcecf4610784632
gintas <>
2005-02-02 18:05:31 +0000

    Created a module for calendar views.

---
+++ src/schoolbell/app/browser/tests/test_cal.py
@@ -0,0 +1,48 @@
+#
+# SchoolTool - common information systems platform for school administration
+# Copyright (c) 2005 Shuttleworth Foundation
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+#
+"""
+Tests for SchoolBell calendaring views.
+
+$Id$
+"""
+
+import unittest
+from zope.testing import doctest
+
+def doctest_PlainCalendarView():
+    """Tests for PlainCalendarView.
+
+        >>> from schoolbell.app.browser.app import PlainCalendarView
+        >>> from schoolbell.app.app import Calendar
+        >>> from zope.publisher.browser import TestRequest
+        >>> cal = Calendar()
+        >>> request = TestRequest()
+        >>> view = PlainCalendarView(cal, request)
+
+    """
+
+def test_suite():
+    suite = unittest.TestSuite()
+    suite.addTest(doctest.DocTestSuite())
+    suite.addTest(doctest.DocTestSuite('schoolbell.app.browser.cal'))
+    return suite
+
+
+if __name__ == '__main__':
+    unittest.main(defaultTest='test_suite')
