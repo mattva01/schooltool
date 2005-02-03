afafd068871f31fc78ee52b74428a908f5645138
jinty <>
2005-02-03 23:52:15 +0000

    Initial test at a advanced setup.py. Only schooltool is close to done so far.

--- setup.py
+++ setup.py
@@ -1,7 +1,8 @@
 #!/usr/bin/env python2.3
 #
 # SchoolTool - common information systems platform for school administration
-# Copyright (c) 2003 Shuttleworth Foundation
+# Copyright (c) 2005    Shuttleworth Foundation,
+#                       Brian Sutherland
 #
 # This program is free software; you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
@@ -26,6 +27,8 @@ SchoolTool setup script.
 #
 
 import sys
+import os
+
 if sys.version_info < (2, 3):
     print >> sys.stderr, '%s: need Python 2.3 or later.' % sys.argv[0]
     print >> sys.stderr, 'Your python is %s' % sys.version
@@ -67,8 +70,82 @@ else:
 
 from distutils.core import setup
 
-# TODO: ask distutils to build Zope 3 extension modules somehow
+# which package are we setting up
+if sys.argv[1] == 'schooltool':
+    package = 'schooltool'
+    sys.argv[1:2] = []
+elif sys.argv[1] == 'schoolbell':
+    package = 'schoolbell'
+    sys.argv[1:2] = []
+else:
+    print >> sys.stderr, ("You must specify the package to build as the first"
+                        " command line option, either schooltool or schoolbell")
+    sys.exit(1)
 
-setup(name="schooltool",
-      version="0.9",
-      package_dir={'': 'src'})
+# Set a default manifest
+if sys.argv[1] == 'sdist':
+    sys.argv[2:2] = ['-t', 'MANIFEST.in.' + package,
+            '-m', 'MANIFEST.' + package]
+
+# TODO: ask distutils to build Zope 3 extension modules somehow
+if package == 'schooltool':
+    # Find the browser data files
+    browser_www = []
+    path = os.path.join('src', 'schooltool', 'browser', 'www')
+    for file in os.listdir(path):
+        if file.endswith('.pt') or file.endswith('.js') \
+                or file.endswith('.png') or file.endswith('.css'):
+            browser_www.append(os.path.join(path, file))
+    # Find the ReST data files
+    rest_www = []
+    path = os.path.join('src', 'schooltool', 'rest', 'www')
+    for file in os.listdir(path):
+        if file.endswith('.pt'):
+            rest_www.append(os.path.join(path, file))
+    # Find the schema data files
+    schemas = []
+    path = os.path.join('src', 'schooltool', 'schema')
+    for file in os.listdir(path):
+        if file.endswith('.xml') or file.endswith('.rng'):
+            schemas.append(os.path.join(path, file))
+    # find the translations
+    translations = []
+    path = os.path.join('src', 'schooltool', 'translation')
+    for root, dirs, files in os.walk(path):
+        mo_files = [os.path.join(root, file) for file in files \
+                if file.endswith('.mo')]
+        if mo_files:
+            translations.append((root, mo_files))
+    # Generate the sampleschool
+    basedir = os.path.abspath(os.path.dirname(sys.argv[0]))
+    sys.path.insert(0, os.path.join(basedir, 'src'))
+    sys.path.insert(0, os.path.join(basedir, 'Zope3', 'src'))
+    import schooltool.clients.datagen
+    # If you change the seed, you will also have to recreate ttconfig.data
+    seed = 'schooltool-m4'
+    schooltool.clients.datagen.main(['datagen', seed])
+    # Setup schooltool
+    setup(name="schooltool",
+        version="0.10",
+        url='http://www.schooltool.org',
+        package_dir={'': 'src'},
+        packages=['schooltool', 'schooltool.interfaces',
+            'schooltool.schema', 'schooltool.translation',
+            'schooltool.rest', 'schooltool.browser',
+            'schooltool.clients'],
+        data_files=[('sampleschool', ['persons.csv', 'groups.csv',
+                'resources.csv', 'timetable.csv']),
+            ('browser/www', browser_www),
+            ('rest/www', rest_www),
+            ('', ['src/schooltool/schema.xml']),
+            ('schemas', schemas)] + translations,
+        scripts=['scripts/import-sampleschool', 'scripts/schooltool',
+            'scripts/schooltool-client'])
+
+elif package == 'schoolbell':
+    setup(name="schoolbell",
+        version="1.0rc1",
+        url='http://www.schooltool.org/schoolbell',
+        package_dir={'': 'src'},
+        packages=['schoolbell', 'schoolbell.relationship',
+            'schooltool.app', 'schooltool.app.browser'])
