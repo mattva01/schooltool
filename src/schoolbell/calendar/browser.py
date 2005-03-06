cd092c9dc49ccf313cc6a4e4f593a89090590a15
bskahan <>
2005-03-06 19:12:15 +0000

    Basic View class for Free/Busy

--- src/schoolbell/calendar/browser.py
+++ src/schoolbell/calendar/browser.py
@@ -59,6 +59,35 @@ CalendarICalendarView can export calendars in iCalendar format
 
      The last line must end in '\r\n' see RFC 2445 4.4.
 
+We can also render just the Free/Busy component of iCal
+
+    >>> from datetime import datetime, timedelta
+    >>> from schoolbell.calendar.simple import ImmutableCalendar
+    >>> from schoolbell.calendar.simple import SimpleCalendarEvent
+    >>> event = SimpleCalendarEvent(datetime(2004, 12, 16, 11, 46, 16),
+    ...                             timedelta(hours=1), "doctests",
+    ...                             location=u"Matar\u00f3",
+    ...                             unique_id="12345678-5432@example.com")
+    >>> calendar = ImmutableCalendar([event])
+
+    >>> from zope.publisher.browser import TestRequest
+    >>> view = CalendarVfbView()
+    >>> view.context = calendar
+    >>> view.request = TestRequest()
+    >>> output = view.show()
+
+    >>> lines = output.splitlines(True)
+    >>> from pprint import pprint
+    >>> pprint(lines)
+    ['BEGIN:VCALENDAR\r\n',
+     'VERSION:2.0\r\n',
+     'PRODID:-//SchoolTool.org/NONSGML SchoolBell//EN\r\n',
+     'METHOD:PUBLISH\r\n',
+     'BEGIN:VFREEBUSY\r\n',
+     'FREEBUSY:20041216T114616/20041216T12461600Z\r\n',
+     'END:VFREEBUSY\r\n',
+     'END:VCALENDAR\r\n']
+
 Register the iCalendar read view in ZCML as
 
     <browser:page
@@ -69,9 +98,19 @@ Register the iCalendar read view in ZCML as
         attribute="show"
         />
 
+Register the VfbView as calendar.vfb
+    <browser:page
+        for="schoolbell.calendar.interfaces.ICalendar"
+        name="calendar.vfb"
+        permission="zope.Public"
+        class="schoolbell.calendar.browser.CalendarVfbView"
+        attribute="show"
+        />
+
 """
 
 from schoolbell.calendar.icalendar import convert_calendar_to_ical
+from schoolbell.calendar.icalendar import convert_calendar_to_vfb
 
 
 class CalendarICalendarView(object):
@@ -86,3 +125,19 @@ class CalendarICalendarView(object):
             request.response.setHeader('Content-Length', len(data))
 
         return data
+
+
+class CalendarVfbView(object):
+    """RFC 2445 (ICalendar) Free/Busy view for calendars."""
+
+    def show(self):
+        data = "\r\n".join(convert_calendar_to_vfb(self.context)) + "\r\n"
+        request = self.request
+        if request is not None:
+            request.response.setHeader('Content-Type',
+                                       'text/calendar; charset=UTF-8')
+            request.response.setHeader('Content-Length', len(data))
+
+        return data
+
+
