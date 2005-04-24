4ed89f38b207f96e6036f38fe26256d1ff2d0e86
bskahan <>
2005-04-24 18:44:30 +0000

    oops

---
+++ src/schooltool/tests/test_uris.py
@@ -0,0 +1,71 @@
+#
+# SchoolTool - common information systems platform for school administration
+# Copyright (c) 2005 Shuttleworth Foundation
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+#
+"""
+Unit tests for schooltool.uris
+
+$Id: test_app.py 3504 2005-04-22 16:34:13Z bskahan $
+"""
+
+import unittest
+from zope.testing import doctest
+from zope.app import zapi
+from zope.interface.verify import verifyObject
+
+def doctest_Section():
+    r"""Tests for URIs
+
+        >>> from schooltool.uris import *
+
+        >>> from schoolbell.relationship.tests import setUp, tearDown
+        >>> setUp()
+        >>> import zope.event
+        >>> old_subscribers = zope.event.subscribers[:]
+        >>> zope.event.subscribers.append(enforceInstructionConstraints)
+
+    We will need some sample persons and sections for the demonstration
+
+        >>> from schoolbell.app.app import Person
+        >>> from schooltool.app import Section
+        >>> jonas = Person()
+        >>> petras = Person()
+        >>> developers = Section()
+        >>> admins = Section()
+
+    There are some constraints: Only objects providing ISection can be
+    Sections.
+
+        >>> Instruction(instructor=jonas, section=petras)
+        Traceback (most recent call last):
+          ...
+        InvalidRelationship: Sections must provide ISection.
+
+        >>> zope.event.subscribers[:] = old_subscribers
+        >>> tearDown()
+
+    """
+
+def test_suite():
+    return unittest.TestSuite([
+                doctest.DocTestSuite(optionflags=doctest.ELLIPSIS),
+                doctest.DocTestSuite('schooltool.uris',
+                                     optionflags=doctest.ELLIPSIS),
+           ])
+
+if __name__ == '__main__':
+    unittest.main(defaultTest='test_suite')
