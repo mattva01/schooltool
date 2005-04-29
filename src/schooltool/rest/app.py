7aa74ec3c3513e792b08b6e7474c243408ad73f7
bskahan <>
2005-04-29 16:59:43 +0000

    establish SchoolToolApplicationView for schooltool rest interface

---
+++ src/schooltool/rest/app.py
@@ -0,0 +1,35 @@
+#
+# SchoolTool - common information systems platform for school administration
+# Copyright (c) 2005 Shuttleworth Foundation
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+#
+"""
+RESTive views for SchoolBellApplication
+
+$Id: app.py 3419 2005-04-14 18:34:36Z alga $
+"""
+from zope.app import zapi
+
+from schoolbell.app.rest import View, Template
+from schoolbell.app.rest.app import ApplicationView
+
+from schooltool import SchoolToolMessageID as _
+
+
+class SchoolToolApplicationView(ApplicationView):
+    """The root view for the application."""
+
+    template = Template("www/app.pt", content_type="text/xml; charset=UTF-8")
---
+++ src/schooltool/rest/tests/test_app.py
@@ -0,0 +1,59 @@
+#
+# SchoolTool - common information systems platform for school administration
+# Copyright (c) 2005 Shuttleworth Foundation
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+#
+"""
+Unit tests for schooltool.app.
+
+$Id: test_app.py 3526 2005-04-28 17:16:47Z bskahan $
+"""
+
+import unittest
+import pprint
+
+from zope.testing import doctest
+from zope.app import zapi
+from zope.interface import directlyProvides, Interface
+from zope.interface.verify import verifyObject
+from zope.app.traversing.interfaces import IContainmentRoot
+from zope.app.testing import setup, ztapi
+from zope.publisher.browser import TestRequest
+
+
+def doctest_SchoolToolApplicationView():
+    """SchoolToolApplication
+
+    Lets create a schooltool instance and make a view for it:
+
+        >>> from schooltool.rest.app import SchoolToolApplicationView
+        >>> from schooltool.app import SchoolToolApplication
+        >>> setup.placefulSetUp()
+        >>> app = SchoolToolApplication()
+        >>> directlyProvides(app, IContainmentRoot)
+        >>> view = SchoolToolApplicationView(app, TestRequest())
+
+    """
+
+def test_suite():
+    return unittest.TestSuite([
+                doctest.DocTestSuite(optionflags=doctest.ELLIPSIS),
+                doctest.DocTestSuite('schooltool.rest.app',
+                                     optionflags=doctest.ELLIPSIS),
+           ])
+
+if __name__ == '__main__':
+    unittest.main(defaultTest='test_suite')
