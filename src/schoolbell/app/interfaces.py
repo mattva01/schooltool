63ba3f31c2f437f6122bf9879f8fd35fa1f963f7
tvon <>
2005-07-06 05:27:02 +0000

    newline

--- src/schoolbell/app/interfaces.py
+++ src/schoolbell/app/interfaces.py
@@ -430,6 +430,7 @@ class ISchoolBellAuthentication(IAuthentication, ILogout):
     def clearCredentials(request):
         """Forget the username and password stored in a session"""
 
+
 class IWriteCalendar(Interface):
 
     def write(data, charset='UTF-8'):
