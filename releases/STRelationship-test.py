7b5997eefada1cdd8ab426dbd2b212ce3403bfe5
srichter <>
2005-08-25 21:17:32 +0000

    Always ignore zope.app

--- releases/STRelationship-test.py
+++ releases/STRelationship-test.py
@@ -8,7 +8,7 @@ def do(command):
 
 do('rm -rf STRelationship-0.0.0*')
 do('../../../zpkgtools/bin/zpkg '
-   '-C STRelationship.cfg STRelationship')
+   '-x zope.app -C STRelationship.cfg STRelationship')
 do('tar xozf STRelationship-0.0.0.tgz')
 os.chdir('STRelationship-0.0.0')
 do('python setup.py install --prefix here')
