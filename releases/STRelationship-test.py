f5bb8251b4b8f9c820aa9d9bc7ac81ce798509dc
srichter <>
2005-08-26 13:42:43 +0000

    Improved script to install in the correct place (iow, use --home instead of --prefix). Also got the tests to run in the test run, so we know that everything necessary is installed.

--- releases/STRelationship-test.py
+++ releases/STRelationship-test.py
@@ -11,4 +11,13 @@ do('../../../zpkgtools/bin/zpkg '
    '-x zope.app -C STRelationship.cfg STRelationship')
 do('tar xozf STRelationship-0.0.0.tgz')
 os.chdir('STRelationship-0.0.0')
-do('python setup.py install --prefix here')
+do('python setup.py install --home `pwd`/here')
+
+zope3_path = os.path.abspath(os.curdir + '/../../Zope3/src')
+do('export PYTHONPATH=%s' %zope3_path)
+
+os.chdir('here/lib/python')
+tests_path = os.path.abspath(os.curdir + '/schooltool/relationship/tests')
+for fn in os.listdir(tests_path):
+    if fn.startswith('test') and fn.endswith('.py'):
+        do('python %s/%s' %(tests_path, fn))
