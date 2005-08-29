b66c5cc8d577f5eccf2f640db3336cf963f4e2a0
srichter <>
2005-08-29 19:13:58 +0000

    Change to new zpkgtools features added by Fred today.

--- releases/STRelationship-test.py
+++ releases/STRelationship-test.py
@@ -8,16 +8,17 @@ def do(command):
 
 do('rm -rf STRelationship-0.0.0*')
 do('../../../zpkgtools/bin/zpkg '
-   '-x zope.app -C STRelationship.cfg STRelationship')
+   '-x zope.app -C STRelationship.cfg')
 do('tar xozf STRelationship-0.0.0.tgz')
 os.chdir('STRelationship-0.0.0')
 do('python setup.py install --home `pwd`/here')
 
 zope3_path = os.path.abspath(os.curdir + '/../../Zope3/src')
-do('export PYTHONPATH=%s' %zope3_path)
+st_path = os.path.abspath(os.curdir + '/here/lib/python')
+exp = 'export PYTHONPATH=%s:%s' %(zope3_path, st_path)
 
 os.chdir('here/lib/python')
 tests_path = os.path.abspath(os.curdir + '/schooltool/relationship/tests')
 for fn in os.listdir(tests_path):
     if fn.startswith('test') and fn.endswith('.py'):
-        do('python %s/%s' %(tests_path, fn))
+        do('%s; python %s/%s' %(exp, tests_path, fn))
