a49b8f65fd93c74773d2e5dfd63b78c483083a4a
mg <>
2005-09-13 18:13:58 +0000

    Playing with statistics: display min/max/mean/stddev.
    Big fat warning: the statistics are probably totally bogus.  I refer you to the
    documentation of the timeit module, and to a nice rant about programmers'
    ignorance of statistics[1].
    
      [1] http://www.zedshaw.com/blog/programming/programmer_stats.html

--- benchmark/benchmark.py
+++ benchmark/benchmark.py
@@ -20,9 +20,23 @@ from schoolbell.app.browser.ftests.test_all import find_ftesting_zcml
 
 def measure(fn):
     """Measure the running time of `fn` in seconds (very crudely)."""
+    # Yes, I know that we're supposed to use the timeit module here, but its
+    # API is inconvenient, and we do not need a lot of precision here.
     t0 = time.time()
     fn()
     return time.time() - t0
+
+
+def mean(seq):
+    """Compute the mean of a sequence."""
+    return sum(seq) / len(seq)
+
+
+def stddev(seq):
+    """Compute the standard deviation of a sequence."""
+    avg = mean(seq)
+    variance = sum((x - avg) ** 2 for x in seq) / len(seq)
+    return variance ** 0.5
 
 
 def benchmark(title, fn, count=5):
@@ -33,6 +47,10 @@ def benchmark(title, fn, count=5):
         time = measure(fn)
         print "  %.3f seconds" % time
         times.append(time)
+    # Note: do not pay much attention to the statistical significance of these
+    # values -- see the timeit module documentation.
+    print ("    min = %.3f, max = %.3f, mean = %.3f, stddev = %.3f"
+           % (min(times), max(times), mean(times), stddev(times)))
 
 
 def http(indented_request_string, http_caller=HTTPCaller()):
