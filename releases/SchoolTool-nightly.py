ee5b7965dcd39d43f9da8526ee23c0db96886ff9
jinty <>
2005-09-30 07:53:03 +0000

    Add a configuration for a minimalistic schooltool tarball that will be the basis for packaging.

---
+++ releases/SchoolTool-nightly.py
@@ -0,0 +1,23 @@
+import os, sys
+
+def do(command):
+    print command
+    if os.system(command):
+        sys.exit(1)
+
+
+do('rm -rf SchoolTool-0.0.0*')
+do('../../../zpkgtools/bin/zpkg -C SchoolTool.cfg')
+do('tar xzf SchoolTool-0.0.0.tgz')
+os.chdir('SchoolTool-0.0.0')
+do('./configure --prefix `pwd`/z --with-python=%s' %sys.executable)
+do('make install')
+os.chdir('z')
+do("bin/schooltooltest -vp1")
+# Use default username and password
+do("bin/mkschooltoolinst -d`pwd`/../i1")
+# Choose a username and password
+do("bin/mkschooltoolinst -d`pwd`/../i2 -u admin:123")
+os.chdir('../i2')
+do("bin/test --testschooltool")
+do('bin/schooltool-server')
