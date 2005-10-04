d81883b558bdf01af1bf8ca35cd551c2c5a340d1
alga <>
2005-10-04 15:22:21 +0000

    Sample data plugins that generate persons.

---
+++ src/schooltool/person/sampledata.py
@@ -0,0 +1,69 @@
+#
+# SchoolTool - common information systems platform for school administration
+# Copyright (c) 2005 Shuttleworth Foundation
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+#
+"""
+Person sample data generation
+
+$Id$
+"""
+
+import random
+
+from zope.interface import implements
+
+from schooltool.sampledata.interfaces import ISampleDataPlugin
+from schooltool.person.person import Person
+from schooltool.group.group import Group
+from schooltool.sampledata.name import NameGenerator
+
+
+class SampleStudents(object):
+
+    implements(ISampleDataPlugin)
+
+    name = 'students'
+    dependencies = ()
+
+    # Number of persons to generate
+    power = 1000
+
+    def generate(self, app, seed=None):
+        namegen = NameGenerator(str(seed) + self.name)
+        for i in range(self.power):
+            name = namegen.generate()
+            app['persons']['student%03d' % i] = Person('student%03d' % i,
+                                                       title=name)
+
+
+class SampleTeachers(object):
+    implements(ISampleDataPlugin)
+
+    name = 'teachers'
+    dependencies = ()
+
+    # Number of teachers to generate
+    power = 48
+
+    def generate(self, app, seed=None):
+        namegen = NameGenerator(str(seed) + self.name)
+        teachers = app['groups']['teachers'] = Group(title='Teachers')
+        for i in range(self.power):
+            name = namegen.generate()
+            person = Person('teacher%03d' % i, title=name)
+            teachers.members.add(person)
+            app['persons']['teacher%03d' % i] = person
---
+++ src/schooltool/person/tests/test_sampledata.py
@@ -0,0 +1,134 @@
+#
+# SchoolTool - common information systems platform for school administration
+# Copyright (c) 2005 Shuttleworth Foundation
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+#
+"""
+Unit tests for schooltool.app.security
+
+$Id$
+"""
+import unittest
+from pprint import pprint
+
+from zope.interface.verify import verifyObject
+from zope.testing import doctest
+from zope.app.testing import setup
+
+from schooltool.testing.setup import setupLocalGrants
+from schooltool.testing import setup as stsetup
+from schooltool.relationship.tests import setUpRelationships
+
+def setUp(test):
+    setup.placefulSetUp()
+
+
+def tearDown(test):
+    setup.placefulTearDown()
+
+
+def doctest_SampleStudents():
+    """A sample data plugin that generates students
+
+        >>> from schooltool.person.sampledata import SampleStudents
+        >>> from schooltool.sampledata.interfaces import ISampleDataPlugin
+        >>> plugin = SampleStudents()
+        >>> verifyObject(ISampleDataPlugin, plugin)
+        True
+
+    This plugin simply creates a 1000 persons with random names and
+    usernames of a form 'student123'.
+
+        >>> plugin.power
+        1000
+
+        >>> app = stsetup.setupSchoolToolSite()
+        >>> len(app['persons'])
+        0
+
+        >>> plugin.generate(app, 42)
+
+        >>> len(app['persons'])
+        1000
+
+        >>> for i in range(5):
+        ...     print app['persons']['student%03d' % i].title
+        Freja Freeman
+        Klara Phillips
+        Cath Rodriguez
+        Daniela Petersen
+        Gabbie Cunningham
+
+    """
+
+
+def doctest_SampleTeachers():
+    """A sample data plugin that generates students
+
+        >>> setUpRelationships()
+
+        >>> from schooltool.person.sampledata import SampleTeachers
+        >>> from schooltool.sampledata.interfaces import ISampleDataPlugin
+        >>> plugin = SampleTeachers()
+        >>> verifyObject(ISampleDataPlugin, plugin)
+        True
+
+    This plugin creates this many teachers:
+
+        >>> plugin.power
+        48
+
+    This plugin creates a Teachers group and a number of teachers.
+
+        >>> app = stsetup.setupSchoolToolSite()
+        >>> len(app['persons'])
+        0
+        >>> len(app['groups'])
+        0
+
+        >>> plugin.generate(app, 42)
+        >>> len(app['groups'])
+        1
+        >>> teachers = app['groups'].values()[0]
+        >>> teachers
+        <schooltool.group.group.Group object at ...>
+        >>> teachers.title
+        'Teachers'
+        >>> len(teachers.members)
+        48
+
+    Teachers' names are different from the students' names above:
+
+        >>> for i in range(5):
+        ...     print app['persons']['teacher%03d' % i].title
+        Catherine Martin
+        Kimmy Cooper
+        Helen Patterson
+        Iris Laurent
+        Philippa Stewart
+
+    """
+
+
+def test_suite():
+    return unittest.TestSuite([
+        doctest.DocTestSuite(setUp=setUp, tearDown=tearDown,
+                             optionflags=doctest.ELLIPSIS),
+        ])
+
+
+if __name__ == '__main__':
+    unittest.main(defaultTest='test_suite')
