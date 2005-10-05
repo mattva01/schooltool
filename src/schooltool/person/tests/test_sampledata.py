67b1f34561c11310d2dd2446d3d21f3d08f60aca
alga <>
2005-10-05 16:35:47 +0000

    Fixed a docstring.

--- src/schooltool/person/tests/test_sampledata.py
+++ src/schooltool/person/tests/test_sampledata.py
@@ -17,7 +17,7 @@
 # Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 #
 """
-Unit tests for schooltool.app.security
+Unit tests for schooltool.person.sampledata
 
 $Id$
 """
