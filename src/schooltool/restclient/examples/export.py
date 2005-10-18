4d35e92e9a9d9ae7e21032f8a514683aed81ba51
ignas <>
2005-10-18 18:33:17 +0000

    Finishing touches for REST import example.

--- src/schooltool/restclient/examples/export.py
+++ src/schooltool/restclient/examples/export.py
@@ -1,19 +1,26 @@
 #!/usr/bin/env python
-"""Script that exports a list of all users into one csv file and
-connection between groups and users into another one.
+"""A script that exports a list of SchoolTool users into one CSV file
+and connections between groups and users into another one.
 
-Formats are like this:
-persons.csv
-username, title
+Fields in persons.csv:
+  username, title
 
-groups.csv
-group_id, title, description, member1, member2, member3
+Fields in groups.csv:
+  group_id, title, description, member1, member2, member3, ...
 
 """
 
+# Configuration
+
+server_address = 'localhost'
+server_port = 7001
+username = 'manager'
+password = 'schooltool'
+
+# End of configuration
+
 import sys
 import os
-import csv
 
 
 if sys.version_info < (2, 3):
@@ -21,6 +28,12 @@ if sys.version_info < (2, 3):
     print >> sys.stderr, 'Your python is %s' % sys.version
     sys.exit(1)
 
+
+import csv    # This is one of the reasons why we need 2.3
+
+
+# SchoolTool should be in the Python path.  Let us make it run
+# directly in the source checkout
 basedir = os.path.normpath(os.path.join(os.path.dirname(__file__),
                                         os.path.pardir,
                                         os.path.pardir,
@@ -29,51 +42,67 @@ basedir = os.path.normpath(os.path.join(os.path.dirname(__file__),
 sys.path.insert(0, os.path.join(basedir, 'src'))
 sys.path.insert(0, os.path.join(basedir, 'Zope3', 'src'))
 
+
 from schooltool.restclient.restclient import SchoolToolClient
 
 
-stc = SchoolToolClient()
-stc.setUser('manager', 'schooltool')
-
 
 def get_id(ref):
-    """Get object id out of it's ref."""
+    """Get object ID from its URL."""
     url = ref.url
     return url.split("/")[-1]
 
 
-def process_persons():
+def process_persons(stc):
+    """Extract a sequence of person IDs and titles from the server.
+
+    Returned strings are in UTF-8.
+    """
     for person_ref in stc.getPersons():
-        person_id = get_id(person_ref)
+        person_id = get_id(person_ref).encode("UTF-8")
         person_title = person_ref.title.encode("UTF-8")
         yield (person_id, person_title)
 
 
-def export_persons(file_name):
-    file = open(file_name, "w")
-    writer = csv.writer(file)
-    writer.writerows(process_persons())
+def export_persons(file_name, persons):
+    """Write a list of person ID and title tuples to a CSV file."""
+    f = open(file_name, "w")
+    writer = csv.writer(f)
+    writer.writerows(persons)
 
 
-def process_groups():
+def process_groups(stc):
+    """Extract a sequence of groups and their members from the server.
+
+    Returned strings are in UTF-8.
+    """
     for group_ref in stc.getGroups():
-        group_id = get_id(group_ref)
+        group_id = get_id(group_ref).encode("UTF-8")
 
         group_info = group_ref.getInfo()
         title = group_info.title.encode("UTF-8")
         description = group_info.description.encode("UTF-8")
 
         member_refs = group_ref.getMembers()
-        member_ids = [get_id(ref) for ref in member_refs]
+        member_ids = [get_id(ref).encode("UTF-8") for ref in member_refs]
 
-        yield tuple([group_id, title, description] + member_ids)
-
-
-def export_groups(file_name):
-    file = open(file_name, "w")
-    writer = csv.writer(file)
-    writer.writerows(process_groups())
+        yield [group_id, title, description] + member_ids
 
 
-export_persons("persons.csv")
-export_groups("groups.csv")
+def export_groups(file_name, groups_and_members):
+    """Write a list of groups and their members to a CSV file."""
+    f = open(file_name, "w")
+    writer = csv.writer(f)
+    writer.writerows(groups_and_members)
+
+
+def main():
+    """Export groups and persons from a SchoolTool server."""
+    stc = SchoolToolClient(server_address, server_port)
+    stc.setUser(username, password)
+    export_persons("persons.csv", process_persons(stc))
+    export_groups("groups.csv", process_groups(stc))
+
+
+if __name__ == '__main__':
+    main()
--- src/schooltool/restclient/examples/import.py
+++ src/schooltool/restclient/examples/import.py
@@ -1,16 +1,41 @@
 #!/usr/bin/env python
-"""Script for importing persons and groups into a running SchoolTool.
+"""A script for importing persons and groups into a running SchoolTool.
+
+It expects to find two CSV files in the current directory: persons.csv
+and groups.csv.
+
+Fields in persons.csv:
+  username, title
+
+Fields in groups.csv:
+  group_id, title, description, member1, member2, member3, ...
+
 """
+
+# Configuration
+
+server_address = 'localhost'
+server_port = 7001
+username = 'manager'
+password = 'schooltool'
+
+# End of configuration
 
 import sys
 import os
-import csv
+
 
 if sys.version_info < (2, 3):
     print >> sys.stderr, '%s: need Python 2.3 or later.' % sys.argv[0]
     print >> sys.stderr, 'Your python is %s' % sys.version
     sys.exit(1)
 
+
+import csv    # This is one of the reasons why we need 2.3
+
+
+# SchoolTool should be in the Python path.  Let us make it run
+# directly in the source checkout
 basedir = os.path.normpath(os.path.join(os.path.dirname(__file__),
                                         os.path.pardir,
                                         os.path.pardir,
@@ -19,25 +44,35 @@ basedir = os.path.normpath(os.path.join(os.path.dirname(__file__),
 sys.path.insert(0, os.path.join(basedir, 'src'))
 sys.path.insert(0, os.path.join(basedir, 'Zope3', 'src'))
 
+
 from schooltool.restclient.restclient import SchoolToolClient
 
-stc = SchoolToolClient()
-stc.setUser('manager', 'schooltool')
 
-persons = {}
 
-def import_persons(file):
-    data = open(file)
+def import_persons(filename, stc):
+    """Read a CSV file and create all persons mentioned therein on the server.
+
+    Expects the CSV file to be in UTF-8.
+
+    Returns a mapping from usernames to PersonRefs.
+    """
+    persons = {}
+    data = open(filename)
     reader = csv.reader(data)
     for row in reader:
         row = [cell.decode('UTF-8') for cell in row]
         username, title = row
         person = stc.createPerson(title, username)
         persons[username] = person
+    return persons
 
 
-def add_members_to_groups(file):
-    data = open(file)
+def add_members_to_groups(filename, stc, persons):
+    """Read a CSV file and create groups with members.
+
+    Expects the CSV file to be in UTF-8.
+    """
+    data = open(filename)
     reader = csv.reader(data)
     for row in reader:
         row = [cell.decode('UTF-8') for cell in row]
@@ -45,11 +80,20 @@ def add_members_to_groups(file):
         group = stc.createGroup(title, description=description,
                                 name=group_id)
         for member_id in row[3:]:
-            person = persons.get(member_id, None)
-            if person:
+            try:
+                person = persons[member_id]
                 group.addMember(person)
-            else:
-                print "Person with id '%s' not found!" % member_id
+            except KeyError:
+                print "Person with id '%s' was not mentioned in persons.csv" % member_id
 
-import_persons("persons.csv")
-add_members_to_groups("groups.csv")
+
+def main():
+    """Import groups and persons to a SchoolTool server."""
+    stc = SchoolToolClient(server_address, server_port)
+    stc.setUser(username, password)
+    persons = import_persons("persons.csv", stc)
+    add_members_to_groups("groups.csv", stc, persons)
+
+
+if __name__ == '__main__':
+    main()
