50fd46a98db19560f0c2944d754b9424f684564d
ignas <>
2006-01-27 15:43:26 +0000

    Sample data for day attendance.

--- src/schooltool/attendance/sampledata.py
+++ src/schooltool/attendance/sampledata.py
@@ -30,6 +30,7 @@ import transaction
 from zope.interface import implements
 from zope.security.proxy import removeSecurityProxy
 
+from schooltool.attendance.interfaces import IDayAttendance
 from schooltool.attendance.interfaces import ISectionAttendance
 from schooltool.sampledata.interfaces import ISampleDataPlugin
 from schooltool.timetable.interfaces import ITimetables
@@ -46,51 +47,102 @@ class SectionAttendancePlugin(object):
     name = "section_attendance"
     dependencies = ("section_timetables", )
 
+    day_absence_rate = 0.15 # A student skips 15% of days on average
+
     absence_rate = 0.03     # A student skips 3% of meetings on average
-    tardy_rate = 0.90       # A student is late to 90% of abscences on average
-    explanation_rate = 0.50 # Student explains 50% of his abscences and tardies
-    excuse_rate = 0.50      # 50% of explained tardies/abscences are excused
-    reject_rate = 0.30      # 30% of explained tardies/abscences are rejected
+    tardy_rate = 0.90       # A student is late to 90% of absences on average
+    explanation_rate = 0.50 # Student explains 50% of his absences and tardies
+    excuse_rate = 0.50      # 50% of explained tardies/absences are excused
+    reject_rate = 0.30      # 30% of explained tardies/absences are rejected
 
     only_last_n_days = None # Instead of generating attendance data
                             # for the whole term (which is slow),
                             # generate it only for the last N days
 
-    def generate(self, app, seed=None):
-        rng = random.Random(seed)
-        term = app['terms']['2005-fall']
+    def explainAttendanceRecord(self, ar):
+        """Sometimes adds an explaination.
 
-        # interval in which attendance records will be generated
-        start_date = term.first
-        end_date = term.last
-        if self.only_last_n_days:
-            start_date = end_date - datetime.timedelta(self.only_last_n_days - 1)
+        Chance of the attendance record being explained is set in
+        explanation_rate.
 
-        for section in app['sections'].values():
-            # You cannot store proxied objects in the ZODB.  It is safe
-            # to unwrap, since only managers can invoke sample data.
-            unproxied_section = removeSecurityProxy(section)
-            meetings = ITimetables(section).makeTimetableCalendar(start_date,
-                                                                  end_date)
+        Depending on excuse_rate and reject_rate the explanation is
+        either accepted or rejected.
+        """
+        if self.rng.random() < self.explanation_rate:
+            ar.addExplanation("My car broke")
+            if self.rng.random() < self.excuse_rate:
+                ar.acceptExplanation()
+            elif self.rng.random() < self.reject_rate:
+                ar.rejectExplanation()
+
+    def generateDayAttendance(self):
+        """Generate sample data for day attendance."""
+        day_absences = {}
+        persons = self.app['persons']
+        for day in self.term:
+            if day < self.start_date:
+                continue
+            if day > self.end_date:
+                continue
+            strdate = day.strftime("%Y-%m-%d")
+            if self.term.isSchoolday(day):
+                day_absences[strdate] = []
+                for person_name in persons:
+                    if not person_name.startswith("student"):
+                        continue
+                    person = persons[person_name]
+                    present = self.rng.random() > self.day_absence_rate
+                    if not present:
+                        day_absences[strdate].append(person)
+                    IDayAttendance(person).record(day, present)
+        transaction.commit()
+        return day_absences
+
+    def generateSectionAttendance(self):
+        """Generate sample data for section attendance."""
+        for section in self.app['sections'].values():
+            meetings = ITimetables(section).makeTimetableCalendar(self.start_date,
+                                                                  self.end_date)
             for meeting in meetings:
                 for student in section.members:
                     attendance = ISectionAttendance(student)
-                    present = rng.random() > self.absence_rate
-                    attendance.record(unproxied_section, meeting.dtstart,
+                    present = self.rng.random() > self.absence_rate
+
+                    datestr = meeting.dtstart.strftime("%Y-%m-%d")
+                    day_absence = (datestr in self.day_absences and
+                                   student in self.day_absences[datestr])
+                    if day_absence:
+                        present = False
+
+                    attendance.record(section, meeting.dtstart,
                                       meeting.duration, meeting.period_id,
                                       present)
-                    if not present:
-                        ar = attendance.get(unproxied_section, meeting.dtstart)
-                        if rng.random() < self.tardy_rate:
+                    ar = attendance.get(section, meeting.dtstart)
+
+                    if not present and not day_absence:
+                        if self.rng.random() < self.tardy_rate:
                             ar.makeTardy(meeting.dtstart + datetime.timedelta(minutes=15))
 
-                        if rng.random() < self.explanation_rate:
-                            ar.addExplanation("My car broke")
-                            if rng.random() < self.excuse_rate:
-                                ar.acceptExplanation()
-                            elif rng.random() < self.reject_rate:
-                                ar.rejectExplanation()
+                    if not present:
+                        self.explainAttendanceRecord(ar)
 
             # The transaction commit keeps the memory usage low, but at a cost
             # of running time and disk space.
             transaction.commit()
+
+    def generate(self, app, seed=None):
+        # You cannot store proxied objects in the ZODB.  It is safe to
+        # unwrap, since only managers can invoke sample data.
+        self.app = removeSecurityProxy(app)
+        self.rng = random.Random(seed)
+        self.term = app['terms']['2005-fall']
+
+        # interval in which attendance records will be generated
+        self.start_date = self.term.first
+        self.end_date = self.term.last
+        if self.only_last_n_days:
+            timedelta = datetime.timedelta(self.only_last_n_days - 1)
+            self.start_date = self.end_date - timedelta
+
+        self.day_absences = self.generateDayAttendance()
+        self.generateSectionAttendance()
--- src/schooltool/attendance/tests/test_sampledata.py
+++ src/schooltool/attendance/tests/test_sampledata.py
@@ -26,6 +26,7 @@ __docformat__ = 'reStructuredText'
 
 import unittest
 import datetime
+import random
 
 from zope.testing import doctest
 from zope.interface.verify import verifyObject
@@ -39,6 +40,16 @@ class TermStub(object):
     first = datetime.date(2005, 9, 1)
     last = datetime.date(2005, 9, 14)
 
+    def __iter__(self):
+        date = self.first
+        while date <= self.last:
+            yield date
+            date += datetime.date.resolution
+
+    def isSchoolday(self, date):
+        return date.day % 2 == 1
+
+
 class SectionStub(object):
     members = ()
     def __init__(self, name):
@@ -46,11 +57,13 @@ class SectionStub(object):
     def __str__(self):
         return self.name
 
+
 class TimetableCalendarEventStub(object):
     def __init__(self, dtstart, duration, period_id):
         self.dtstart = dtstart
         self.duration = duration
         self.period_id = period_id
+
 
 class FakeTimetablesAdapter(object):
     def __init__(self, section):
@@ -69,45 +82,154 @@ class FakeTimetablesAdapter(object):
 
 class FakeAttendanceRecord(object):
     def __init__(self):
-        pass
+        self.hasExplanations = False
+        self.accepted = False
+        self.rejected = False
+
     def makeTardy(self, datetime):
         print "Tardy at %s" % datetime
 
     def addExplanation(self, explanation):
-        print "Added explanation '%s'" % explanation
+        self.hasExplanations = True
+
+    def acceptExplanation(self):
+        self.accepted = True
+
     def rejectExplanation(self):
-        print "Rejected explanation."
+        self.rejected = True
 
 
 class FakeAttendanceAdapter(object):
     def __init__(self, person):
         self.person = person
+
     def record(self, section, dtstart, duration, period_id, present):
         print "%s was %s on %s (%s, %s)" % (self.person,
                     {True: 'present', False: 'absent'}[present], dtstart,
                     period_id, section)
+
     def get(self, section, datetime):
         return FakeAttendanceRecord()
 
 
-def doctest_SectionAttendancePlugin():
-    r"""Tests for SectionAttendancePlugin
+class FakeDayAttendanceAdapter(object):
+    def __init__(self, person):
+        self.person = person
 
-        >>> from schooltool.attendance.sampledata \
-        ...     import SectionAttendancePlugin
+    def record(self, date, present):
+        print "%s was %s on %s" % (self.person,
+                                   {True: 'present',
+                                    False: 'absent'}[present],
+                                   date)
+
+def doctest_SectionAttendancePlugin_explainAttendanceRecord():
+    """Tests for SectionAttendancePlugin.explainAttendanceRecord.
+
+        >>> from schooltool.attendance.sampledata import SectionAttendancePlugin
         >>> plugin = SectionAttendancePlugin()
-        >>> verifyObject(ISampleDataPlugin, plugin)
-        True
+
+    Whether an attendance record will be explained or not depends on
+    the explanation_rate.
+
+        >>> plugin.rng = random.Random(42)
+        >>> plugin.explanation_rate = 1
+        >>> for i in range(100):
+        ...     ar = FakeAttendanceRecord()
+        ...     plugin.explainAttendanceRecord(ar)
+        ...     assert(ar.hasExplanations)
+
+    The explanation being accepted depends on the excuse_rate:
+
+        >>> plugin.excuse_rate = 1
+        >>> for i in range(100):
+        ...     ar = FakeAttendanceRecord()
+        ...     plugin.explainAttendanceRecord(ar)
+        ...     assert(ar.hasExplanations)
+        ...     assert(ar.accepted)
+
+     Rejection depends on reject_rate:
+
+        >>> plugin.excuse_rate = 0
+        >>> plugin.reject_rate = 1
+        >>> for i in range(100):
+        ...     ar = FakeAttendanceRecord()
+        ...     plugin.explainAttendanceRecord(ar)
+        ...     assert(ar.hasExplanations)
+        ...     assert(ar.rejected)
+
+    """
+
+
+def doctest_SectionAttendancePlugin_generateDayAttendance():
+    """Tests for SectionAttendancePlugin.generateDayAttendance
+
+        >>> from schooltool.attendance.sampledata import SectionAttendancePlugin
+        >>> plugin = SectionAttendancePlugin()
+
+        >>> app = {'terms': {'2005-fall': TermStub()},
+        ...        'persons': {'student_Jon': 'Jon',
+        ...                    'student_Ian': 'Ian',
+        ...                    'teacher_Ann': 'Ann'}}
+        >>> plugin.app = app
+        >>> plugin.term = app['terms']['2005-fall']
+        >>> plugin.start_date = plugin.term.last - datetime.timedelta(days=3)
+        >>> plugin.end_date = plugin.term.last
+        >>> plugin.rng = random.Random(42)
+        >>> plugin.day_absence_rate = 1
+
+
+    Persons need to be adaptable to IDayAttendance
+
+        >>> from schooltool.attendance.interfaces import IDayAttendance
+        >>> ztapi.provideAdapter(None, IDayAttendance,
+        ...                      FakeDayAttendanceAdapter)
+
+    Only students are marked as absent/present:
+
+        >>> print sorted(plugin.generateDayAttendance().items())
+        Jon was absent on 2005-09-11
+        Ian was absent on 2005-09-11
+        Jon was absent on 2005-09-13
+        Ian was absent on 2005-09-13
+        [('2005-09-11', ['Jon', 'Ian']),
+         ('2005-09-13', ['Jon', 'Ian'])]
+
+    Unless the abscence rate is 0:
+
+        >>> plugin.day_absence_rate = 0
+        >>> print sorted(plugin.generateDayAttendance().items())
+        Jon was present on 2005-09-11
+        Ian was present on 2005-09-11
+        Jon was present on 2005-09-13
+        Ian was present on 2005-09-13
+        [('2005-09-11', []),
+         ('2005-09-13', [])]
+
+    """
+
+
+def doctest_SectionAttendancePlugin_generateSectionAttendance():
+    """Tests for SectionAttendancePlugin.generateDayAttendance
+
+        >>> from schooltool.attendance.sampledata import SectionAttendancePlugin
+        >>> plugin = SectionAttendancePlugin()
 
     The plugin wants an application object with 'sections' and 'terms'
     containers.  It is easier to use stubs:
 
-        >>> app = {'terms': {'2005-fall': TermStub()},
+        >>> term = TermStub()
+        >>> app = {'terms': {'2005-fall': term},
         ...        'sections': {'s1': SectionStub('s1'),
         ...                     's2': SectionStub('s2')}}
 
         >>> app['sections']['s1'].members = ['Jon', 'Ian']
         >>> app['sections']['s2'].members = ['Ann']
+        >>> plugin.app = app
+        >>> plugin.term = term
+        >>> plugin.start_date = term.first
+        >>> plugin.end_date = term.last
+        >>> plugin.rng = random.Random(42)
+        >>> plugin.day_absences = []
 
     The "sections" need to be adaptable to ITimetables
 
@@ -121,11 +243,10 @@ def doctest_SectionAttendancePlugin():
 
     Now we can generate some sample data
 
-        >>> plugin.generate(app, seed=42)
+        >>> plugin.generateSectionAttendance()
         Ann was present on 2005-09-01 09:30:00 (p1, s2)
         Ann was absent on 2005-09-03 09:30:00 (p1, s2)
         Tardy at 2005-09-03 09:45:00
-        Added explanation 'My car broke'
         Ann was present on 2005-09-05 09:30:00 (p1, s2)
         Ann was present on 2005-09-07 09:30:00 (p1, s2)
         Ann was present on 2005-09-09 09:30:00 (p1, s2)
@@ -149,19 +270,100 @@ def doctest_SectionAttendancePlugin():
         Jon was present on 2005-09-13 09:30:00 (p1, s1)
         Ian was present on 2005-09-13 09:30:00 (p1, s1)
 
-    You can generate sample data only for the last days of the term:
+    Let's shift the start date a bit, and reset the rng:
 
-        >>> plugin.only_last_n_days = 3
-        >>> plugin.generate(app, seed=15)
-        Ann was present on 2005-09-12 09:30:00 (p1, s2)
-        Ann was absent on 2005-09-14 09:30:00 (p1, s2)
-        Tardy at 2005-09-14 09:45:00
-        Added explanation 'My car broke'
-        Rejected explanation.
-        Jon was present on 2005-09-12 09:30:00 (p1, s1)
-        Ian was present on 2005-09-12 09:30:00 (p1, s1)
-        Jon was present on 2005-09-14 09:30:00 (p1, s1)
-        Ian was present on 2005-09-14 09:30:00 (p1, s1)
+        >>> plugin.start_date = term.last - datetime.timedelta(days=3)
+        >>> plugin.rng = random.Random(42)
+        >>> plugin.generateSectionAttendance()
+        Ann was present on 2005-09-11 09:30:00 (p1, s2)
+        Ann was absent on 2005-09-13 09:30:00 (p1, s2)
+        Tardy at 2005-09-13 09:45:00
+        Jon was present on 2005-09-11 09:30:00 (p1, s1)
+        Ian was present on 2005-09-11 09:30:00 (p1, s1)
+        Jon was present on 2005-09-13 09:30:00 (p1, s1)
+        Ian was absent on 2005-09-13 09:30:00 (p1, s1)
+        Tardy at 2005-09-13 09:45:00
+
+    If there was a day abscence - the student should not be present in
+    any of the section events (even if absence_rate is zero):
+
+        >>> plugin.absence_rate = 0
+        >>> plugin.rng = random.Random(42)
+        >>> plugin.day_absences = {'2005-09-11': ['Jon'],
+        ...                        '2005-09-13': ['Ian']}
+        >>> plugin.generateSectionAttendance()
+        Ann was present on 2005-09-11 09:30:00 (p1, s2)
+        Ann was present on 2005-09-13 09:30:00 (p1, s2)
+        Jon was absent on 2005-09-11 09:30:00 (p1, s1)
+        Ian was present on 2005-09-11 09:30:00 (p1, s1)
+        Jon was present on 2005-09-13 09:30:00 (p1, s1)
+        Ian was absent on 2005-09-13 09:30:00 (p1, s1)
+
+    """
+
+
+def doctest_SectionAttendancePlugin_generate():
+    r"""Tests for SectionAttendancePlugin
+
+        >>> from schooltool.attendance.sampledata \
+        ...     import SectionAttendancePlugin
+        >>> plugin = SectionAttendancePlugin()
+        >>> verifyObject(ISampleDataPlugin, plugin)
+        True
+
+    Generate just sets up some attributes of the plugin and delegates
+    the generation of data to it's methods.
+
+    Let's stub those methods:
+
+        >>> def generateDayAttendanceStub():
+        ...     print 'Generating day attendance data'
+        ...     return 'day_attendance_data'
+        >>> def generateSectionAttendanceStub():
+        ...     print 'Generating section attendance data'
+        >>> plugin.generateDayAttendance = generateDayAttendanceStub
+        >>> plugin.generateSectionAttendance = generateSectionAttendanceStub
+
+        >>> term = TermStub()
+        >>> app = {'terms': {'2005-fall': term}}
+
+    And generate the sample data:
+
+        >>> plugin.generate(app, 42)
+        Generating day attendance data
+        Generating section attendance data
+
+        >>> plugin.app is app
+        True
+
+        >>> plugin.term is term
+        True
+
+        >>> plugin.start_date
+        datetime.date(2005, 9, 1)
+
+        >>> plugin.end_date
+        datetime.date(2005, 9, 14)
+
+        >>> plugin.rng
+        <random.Random object at ...>
+
+        >>> plugin.day_absences
+        'day_attendance_data'
+
+    Now if we set the only_last_n_days attribute, the start_date
+    should get shifted:
+
+        >>> plugin.only_last_n_days = 2
+        >>> plugin.generate(app, 42)
+        Generating day attendance data
+        Generating section attendance data
+
+        >>> plugin.start_date
+        datetime.date(2005, 9, 13)
+
+        >>> plugin.end_date
+        datetime.date(2005, 9, 14)
 
     """
 
