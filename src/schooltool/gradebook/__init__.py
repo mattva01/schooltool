46278942354351f66d3fbd753d1f6547d5569fd5
faassen <>
2006-02-08 18:33:59 +0000

    We don't want to security proxy Decimals, as unfortunately Decimals do isinstance checks on the Decimal itself, which breaks in the face of Zope 3 security proxies. We've therefore done an insecure thing if untrusted code is ever to be written with Schooltool: we've turned Decimal into an unproxied rock.

--- src/schooltool/gradebook/__init__.py
+++ src/schooltool/gradebook/__init__.py
@@ -13,3 +13,13 @@ def registerTestSetup():
 
 registerTestSetup()
 del registerTestSetup
+
+def makeDecimalARock():
+    # XXX this is insecure
+    from decimal import Decimal
+    from zope.security.checker import NoProxy
+    import zope.security
+    zope.security.checker.BasicTypes[Decimal] = NoProxy
+
+makeDecimalARock()
+del makeDecimalARock
