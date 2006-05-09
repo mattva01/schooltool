36705b007260ca2ca1ee22a8ebc14009cb87a709
faassen <>
2006-05-09 16:48:27 +0000

    Add simple display form based on formlib.

--- src/schooltool/demographics/browser/personform.py
+++ src/schooltool/demographics/browser/personform.py
@@ -8,13 +8,18 @@ from schooltool.demographics import interfaces
 
 nameinfo_traverser = SingleAttributeTraverserPlugin('nameinfo')
 
+class PageDisplayForm(form.PageDisplayForm):
+    template = ViewPageTemplateFile('display_form.pt')
+
 class PageEditForm(form.PageEditForm):
     template = ViewPageTemplateFile('edit_form.pt')
-    
+
 class NameInfoEdit(PageEditForm):
     form_fields = form.Fields(interfaces.INameInfo)
 
+class NameInfoDisplay(PageDisplayForm):
+    form_fields = form.Fields(interfaces.INameInfo)
+    
 class PersonAddView(PersonAddViewBase):
     _factory = Person
 
-    
