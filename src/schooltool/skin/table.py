d889ca73a713c83d9dab5601e61c95a9139ef089
faassen <>
2006-05-30 12:30:46 +0000

    Move more generic table code into skin.

---
+++ src/schooltool/skin/table.py
@@ -0,0 +1,66 @@
+from zope.publisher.browser import BrowserPage
+from zope.app.pagetemplate import ViewPageTemplateFile
+from zc.table import table
+from schooltool.batching import Batch
+
+class TablePage(BrowserPage):
+    """Base class to easily created table-driven views.
+
+    Has support for batching and sorting.
+
+    Subclass and define columns() and values() to make this work for
+    your own data. columns() must return a list of zc.table column
+    objects and values() must return an iterable of objects in the
+    table.
+    """
+    
+    __call__ = ViewPageTemplateFile('templates/table.pt')
+    
+    def __init__(self, context, request):
+        super(TablePage, self).__init__(context, request)
+        self.batch_start = int(request.form.get('batch_start', 0))
+        self.batch_size = int(request.form.get('batch_size', 10))
+        self._cached_values = None
+        
+    def table(self):
+        formatter = table.StandaloneFullFormatter(
+            self.context, self.request, self.cached_values(),
+            columns=self.columns(),
+            batch_start=self.batch_start, batch_size=self.batch_size,
+            sort_on=self.sortOn())
+        # set CSS class for the zc.table generated tables, to differentiate it
+        # from other tables.
+        formatter.cssClasses['table'] = 'data'
+        return formatter()
+
+    def batch(self):
+        # XXX note that the schooltool.batching system is *only* used to
+        # provide enough information for the batch navigation macros. We
+        # actually use the zc.table system for the actual batching
+        # bit
+        return Batch(self.cached_values(), self.batch_start, self.batch_size)
+
+    def cached_values(self):
+        if self._cached_values is None:
+            self._cached_values = self.values()
+        return self._cached_values
+    
+    def values(self):
+        raise NotImplementedError
+
+    def columns(self):
+        raise NotImplementedError
+    
+    def extraUrl(self):
+        return self.sortOptions()
+
+    def sortOptions(self):
+        sort_on = self.request.form.get('sort_on', None)
+        if not sort_on:
+            return ''
+        l = ['sort_on:list=%s' % o for o in sort_on]
+        return '&' + '&'.join(l)
+
+    def sortOn(self):
+        """ Default sort on. """
+        return ()
