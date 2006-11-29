5c431d09f84b105a274d3365ed389fbd3809df8b
ignas <>
2006-11-29 15:37:28 +0000

    Use the FormFullFormatter in the relationship edit views.

--- src/schooltool/app/browser/app.py
+++ src/schooltool/app/browser/app.py
@@ -255,7 +255,7 @@ class RelationshipViewBase(BrowserView):
                                      getter=lambda i, f: i.title,
                                      subsort=True)]
         columns.extend(self.columns())
-        formatter = table.StandaloneFullFormatter(
+        formatter = table.FormFullFormatter(
             self.context, self.request, self.filter(self.getAvailableItems()),
             columns=columns,
             batch_start=self.batch_start, batch_size=self.batch_size,
@@ -273,7 +273,7 @@ class RelationshipViewBase(BrowserView):
                                      getter=lambda i, f: i.title,
                                      subsort=True)]
         columns.extend(self.columns())
-        formatter = table.StandaloneFullFormatter(
+        formatter = table.FormFullFormatter(
             self.context, self.request, self.getSelectedItems(),
             columns=columns,
             sort_on=self.sortOn(),
