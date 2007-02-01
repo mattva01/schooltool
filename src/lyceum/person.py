5bb7b1caa8e4cfd1d9d0f4f5e9eab4b15a56afdd
ignas <>
2007-02-01 17:35:50 +0000

    Allow teachers to access calendars of their students.

--- src/lyceum/person.py
+++ src/lyceum/person.py
@@ -1,16 +1,48 @@
+#
+# SchoolTool - common information systems platform for school administration
+# Copyright (c) 2007 Shuttleworth Foundation
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+#
+"""
+Lyceum person specific code.
+
+$Id$
+
+"""
 from zope.interface import implements
+from zope.component import adapts
 from zope.interface import directlyProvides
+from zope.interface import Interface
 
 from zc.table.interfaces import ISortableColumn
-from zc.table.column import GetterColumn
 
 from schooltool.person.person import Person
 from schooltool.person.interfaces import IPersonFactory
+from schooltool.course.section import PersonInstructorsCrowd
+from schooltool.person.person import PersonCalendarCrowd
 from schooltool.skin.table import LocaleAwareGetterColumn
+from lyceum import LyceumMessage as _
+
+
+class ILyceumPerson(Interface):
+    """Marker interface for Lyceum specific person."""
 
 
 class LyceumPerson(Person):
-    pass
+    implements(ILyceumPerson)
 
 
 class PersonFactoryUtility(object):
@@ -36,3 +68,12 @@ class PersonFactoryUtility(object):
     def __call__(self, *args, **kw):
         result = LyceumPerson(*args, **kw)
         return result
+
+
+class LyceumPersonCalendarCrowd(PersonCalendarCrowd):
+    """Crowd that allows instructor of a person access persons calendar"""
+    adapts(ILyceumPerson)
+
+    def contains(self, principal):
+        return (PersonCalendarCrowd.contains(self, principal) or
+                PersonInstructorsCrowd(self.context).contains(principal))
