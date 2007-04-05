50c00dca342f7db1d224000634cb9ae4c876e43b
ignas <>
2007-04-05 14:10:55 +0000

    Make preference link only appear when you can edit preferences.

--- src/schooltool/person/browser/person.py
+++ src/schooltool/person/browser/person.py
@@ -43,6 +43,8 @@ from zope.interface import implements
 from zope.app.pagetemplate import ViewPageTemplateFile
 from zope.security.management import checkPermission
 from zope.viewlet.viewlet import ViewletBase
+from zope.component import queryAdapter
+from zope.component import adapts
 
 from schooltool import SchoolToolMessage as _
 from schooltool.skin.form import BasicForm
@@ -55,6 +57,8 @@ from schooltool.widget.password import PasswordConfirmationWidget
 from schooltool.traverser.traverser import AdapterTraverserPlugin
 from schooltool.skin.table import FilterWidget
 from schooltool.skin.containers import TableContainerView
+from schooltool.securitypolicy.crowds import Crowd
+from schooltool.securitypolicy.interfaces import ICrowd
 
 
 def SourceMultiCheckBoxWidget(field, request):
@@ -439,3 +443,22 @@ class PersonView(BrowserView):
 
     def __init__(self, context, request):
         BrowserView.__init__(self, context, request)
+
+
+class IPreferencesMenuViewlet(Interface):
+    """Marker interface so we could use custom crowd for Preferences menu item"""
+
+
+class PreferencesActionMenuViewlet(object):
+    implements(IPreferencesMenuViewlet)
+
+
+class PreferencesMenuViewletCrowd(Crowd):
+    adapts(IPreferencesMenuViewlet)
+
+    def contains(self, principal):
+        """Returns true if you have the permission to see the calendar."""
+        crowd = queryAdapter(IPersonPreferences(self.context.context),
+                             ICrowd,
+                             name="schooltool.view")
+        return crowd.contains(principal)
