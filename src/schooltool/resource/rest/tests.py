d19db7a145be615e95de93f8ac27b56f4e24706c
ignas <>
2007-04-06 12:20:07 +0000

    Remove commented out test.

--- src/schooltool/resource/rest/tests.py
+++ src/schooltool/resource/rest/tests.py
@@ -204,32 +204,6 @@ class TestResourceView(ApplicationObjectViewTestMixin, unittest.TestCase):
                         xlink:title="Calendar ACL" xlink:type="simple"/>
                </resource>""")
 
-##     def testGETIsLocation(self):
-
-##         self.testObject.isLocation = True
-
-##         result, response = self.get()
-
-##         self.assertEquals(response.getHeader('content-type'),
-##                           "text/xml; charset=UTF-8")
-##         self.assertEqualsXML(result,
-##             """<resource xmlns:xlink="http://www.w3.org/1999/xlink">
-##                    <title>Root resource</title>
-##                    <description/>
-##                    <isLocation>True</isLocation>
-##                    <relationships xlink:href="http://127.0.0.1/resources/root/relationships"
-##                                   xlink:title="Relationships" xlink:type="simple"/>
-##                    <acl xlink:href="http://127.0.0.1/resources/root/acl" xlink:title="ACL"
-##                         xlink:type="simple"/>
-##                    <calendar xlink:href="http://127.0.0.1/resources/root/calendar"
-##                              xlink:title="Calendar" xlink:type="simple"/>
-##                    <relationships
-##                                   xlink:href="http://127.0.0.1/resources/root/calendar/relationships"
-##                                   xlink:title="Calendar subscriptions" xlink:type="simple"/>
-##                    <acl xlink:href="http://127.0.0.1/resources/root/calendar/acl"
-##                         xlink:title="Calendar ACL" xlink:type="simple"/>
-##                </resource>""")
-
 
 def test_suite():
     suite = unittest.TestSuite()
