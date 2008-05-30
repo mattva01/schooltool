f22906cccce02ec0e5f86b7daf86b93c7e6c0f85
Ignas Mikalajūnas <ignas@pov.lt>
2008-05-30 20:00:20 +0300

    Get rid of schooltool and deprecated reimports.

--- src/schoolbell/relationship/uri.py
+++
@@ -1,7 +0,0 @@
-# Just for BBB:
-from zope.deprecation import deprecated
-
-from schooltool.relationship.uri import URIObject
-deprecated(('URIObject',),
-           'This class has moved to schooltool.relationship.uri. '
-           'The reference will be gone in 0.15')
