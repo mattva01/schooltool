c0f5d5852810017835a288f8df785e6f3c1ce598
Ignas Mikalajūnas <ignas@pov.lt>
2008-10-30 17:42:36 +0200

    Hide next school year from non-manager users.

--- src/schooltool/schoolyear/browser/schoolyear.py
+++ src/schooltool/schoolyear/browser/schoolyear.py
@@ -22,6 +22,7 @@ Views for school years and school year container implementation
 from zope.viewlet.viewlet import ViewletBase
 from zope.publisher.interfaces.browser import IBrowserRequest
 from zope.component import adapts
+from zope.security import checkPermission
 from zope.schema import Date, TextLine
 from zope.interface import implements
 from zope.interface import Interface
@@ -328,4 +329,6 @@ class ActiveSchoolYears(ViewletBase):
 
     def nextSchoolYear(self):
         """Return the next school year."""
-        return ISchoolYearContainer(ISchoolToolApplication(None)).getNextSchoolYear()
+        syc = ISchoolYearContainer(ISchoolToolApplication(None))
+        if checkPermission("schooltool.edit", syc):
+            return syc.getNextSchoolYear()
