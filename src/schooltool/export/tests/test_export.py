1a441d04f72aca4893b6b41c86c6e4b7c8646ab6
Ignas Mikalajūnas <ignas@pov.lt>
2009-04-10 11:45:07 +0300

    Update texts in XLS import view, change "Excel import" action to "XLS import".

--- src/schooltool/export/tests/test_export.py
+++ src/schooltool/export/tests/test_export.py
@@ -17,7 +17,7 @@
 # Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 #
 """
-Tests for SchoolTool excel export views.
+Tests for SchoolTool XLS export views.
 """
 import unittest
 from datetime import date
