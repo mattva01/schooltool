d274bbfe3c8c88fed942e9ddfc392492f4de5738
Justas Sadzevicius <justas@pov.lt>
2011-05-30 14:48:32 +0300

    viewlet and viewletManager directives.

---
+++ src/schooltool/skin/flourish/zcml.py
@@ -0,0 +1,236 @@
+#
+# SchoolTool - common information systems platform for school administration
+# Copyright (c) 2011 Shuttleworth Foundation
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+#
+"""
+SchoolTool flourish zcml directives.
+"""
+import zope.browserpage.metadirectives
+import zope.component.zcml
+import zope.configuration.fields
+import zope.schema
+import zope.security.checker
+import zope.viewlet.metadirectives
+from zope.interface import Interface, classImplements
+from zope.configuration.exceptions import ConfigurationError
+from zope.publisher.interfaces.browser import IBrowserRequest
+
+from schooltool.app.browser.meta import contentDirective
+from schooltool.app.browser.meta import subclass_content
+from schooltool.app.browser.meta import handle_interfaces, handle_security
+from schooltool.skin.flourish import interfaces
+from schooltool.skin.flourish.page import Page
+from schooltool.skin.flourish.viewlet import Viewlet, ViewletManager
+from schooltool.common import SchoolToolMessage as _
+
+
+class IRenderOverrides(Interface):
+
+    update = zope.configuration.fields.PythonIdentifier(
+        title=u"The name of the view attribute implementing content update.",
+        required=False,
+        )
+
+    render = zope.configuration.fields.PythonIdentifier(
+        title=u"The name of the view attribute that renders the content.",
+        required=False,
+        )
+
+
+class IViewletOrder(Interface):
+
+    after = zope.configuration.fields.Tokens(
+        title=_("Display this viewlet after the specified viewlets."),
+        required=False)
+
+    before = zope.configuration.fields.Tokens(
+        title=_("Display this viewlet before the specified viewlets."),
+        required=False)
+
+
+class IViewletDirective(zope.viewlet.metadirectives.IViewletDirective,
+                        IRenderOverrides,
+                        IViewletOrder):
+    """A viewlet directive."""
+
+
+# Arbitrary keys and values are allowed to be passed to the viewlet.
+IViewletDirective.setTaggedValue('keyword_arguments', True)
+
+
+class IManagerDirective(zope.viewlet.metadirectives.IViewletManagerDirective,
+                        IViewletOrder):
+    """Viewlet manager directive."""
+
+
+# Arbitrary keys and values are allowed to be passed to the manager.
+IManagerDirective.setTaggedValue('keyword_arguments', True)
+
+
+class IPageDirective(zope.browserpage.metadirectives.IPagesDirective,
+                     IRenderOverrides):
+
+    name = zope.schema.TextLine(
+        title=u"The name of the page (view)",
+        required=True,
+        )
+
+    title = zope.configuration.fields.MessageID(
+        title=u"Title of this page",
+        required=False,
+        )
+
+    subtitle = zope.configuration.fields.MessageID(
+        title=u"Subitle of this page",
+        description=u"""
+            A very short description of this page.
+        """,
+        required=False,
+        )
+
+    page_template = zope.configuration.fields.Path(
+        title=u"Template for the page.",
+        description=u"""
+        Change template that renders the page part between the header
+        and the footer.
+        """,
+        required=False,
+        )
+
+    content_template = zope.configuration.fields.Path(
+        title=u"Template for the main page content.",
+        description=u"""
+        Set template that renders main content for this page.
+        """,
+        required=False,
+        )
+
+
+def viewletManager(
+    _context, name, permission,
+    for_=Interface, layer=interfaces.IFlourishLayer, view=interfaces.IPage,
+    provides=interfaces.IViewletManager,
+    class_=ViewletManager, template=None,
+    update='update', render='render',
+    allowed_interface=(), allowed_attributes=(),
+    **kwargs):
+
+    if not interfaces.IViewletManager.implementedBy(class_):
+        class_ = type(name, (class_, ViewletManager), {})
+    allowed_interface = (tuple(allowed_interface) +
+                         (interfaces.IViewletManager, ))
+    if not provides.implementedBy(class_):
+        classImplements(class_, provides)
+
+    contentDirective(
+        _context, name, permission,
+        for_=for_, layer=layer, view=view,
+        class_=class_, template=template,
+        update=update, render=render,
+        allowed_interface=allowed_interface,
+        allowed_attributes=allowed_attributes,
+        **kwargs)
+
+
+def viewlet(
+    _context, name, permission,
+    for_=Interface, layer=interfaces.IFlourishLayer, view=interfaces.IPage,
+    manager=interfaces.IViewletManager,
+    class_=Viewlet, template=None,
+    update='update', render='render',
+    allowed_interface=None, allowed_attributes=None,
+    **kwargs):
+
+    if not interfaces.IViewlet.implementedBy(class_):
+        class_ = type(name, (class_, Viewlet), {})
+    allowed_interface = (tuple(allowed_interface) +
+                         (interfaces.IViewlet, ))
+
+    if (render == 'render' and
+        class_.render == Viewlet.render):
+        if template:
+            render = 'template'
+        else:
+            raise ConfigurationError("When template and render not specified, "
+                                     "class must implement 'render' method")
+
+    class_ = subclass_content(
+        class_, name, update, render,
+        {'template': template}, kwargs)
+
+    handle_interfaces(_context, (for_, view))
+    handle_interfaces(_context, allowed_interface)
+
+    handle_security(class_, permission,
+                    allowed_interface, allowed_attributes)
+
+    _context.action(
+        discriminator=('schooltool.skin.flourish.viewlet',
+                       for_, layer, view, manager, name),
+        callable=zope.component.zcml.handler,
+        args=('registerAdapter',
+              class_,
+              (for_, layer, view, manager),
+              interfaces.IViewlet,
+              name,
+              _context.info),)
+
+
+def page(_context, name, permission,
+         for_=Interface, layer=interfaces.IFlourishLayer,
+         title=None, subtitle=None,
+         template=None, page_template=None, content_template=None,
+         class_=Page,
+         update='update', render='render',
+         allowed_interface=(), allowed_attributes=(),
+         ):
+
+    if not interfaces.IPage.implementedBy(class_):
+        class_ = type(name, (class_, Page), {})
+
+    allowed_interface = (tuple(allowed_interface) +
+                         (interfaces.IPage, ))
+
+    class_dict = {'__name__': name}
+    if title is not None:
+        class_dict['title'] = title
+    if subtitle is not None:
+        class_dict['subtitle'] = subtitle
+
+    # XXX: raise ConfigurationError if class_ is Page and
+    #      no templates specified
+
+    class_ = subclass_content(
+        class_, name, update, render,
+        {'template': template,
+         'page_template': page_template,
+         'content_template': content_template},
+        class_dict,
+        )
+
+    handle_interfaces(_context, (for_,))
+    handle_interfaces(_context, allowed_interface)
+
+    handle_security(class_, permission,
+                    allowed_interface, allowed_attributes)
+
+    _context.action(
+        discriminator=('view', for_, name, IBrowserRequest, layer),
+        callable=zope.component.zcml.handler,
+        args=('registerAdapter',
+              class_, (for_, layer), Interface, name, _context.info),
+        )
