17cc73e60cffb71abccd9ed499152d34135389a7
Gediminas Paulauskas <menesis@pov.lt>
2011-06-08 18:12:42 +0300

    Made zope.app.apidoc dependency optional: - Add apidoc extra that enables Introspector and API Docs. - Also removes dependency on zope.app.onlinehelp, zope.app.preference,   zope.preference, zope.app.tree.
    If you want to use Introspector:
    - Edit instance/schooltool.conf and add "devmode on" at the bottom
    - Edit base.cfg and add apidoc extra to the schooltool egg:
      [scripts]
      eggs = schooltool [apidoc]
    
    apidoc is left on for now

--- src/schooltool/devmode/__init__.py
+++ src/schooltool/devmode/__init__.py
@@ -3,5 +3,8 @@
 # Due to a different structure, the apidoc BASEDIR is no good.
 import schooltool.app
 from os.path import dirname
-from zope.app.apidoc import utilities
-utilities.BASEDIR = dirname(dirname(dirname(dirname(schooltool.app.__file__))))
+try:
+    from zope.app.apidoc import utilities
+    utilities.BASEDIR = dirname(dirname(dirname(dirname(schooltool.app.__file__))))
+except ImportError:
+    pass
