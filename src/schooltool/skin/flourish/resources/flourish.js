3aca9269ebe8ab28e3fc06fea3b6e2452b00a14b
Alan Elkner <aelkner@gmail.com>
2011-06-26 23:37:20 -0400

    changed call_dialog to calculate width before calling dialog

--- src/schooltool/skin/flourish/resources/flourish.js
+++ src/schooltool/skin/flourish/resources/flourish.js
@@ -9,12 +9,13 @@ $(document).ready(function() {
 
 function call_dialog(url, form_id) {
     $(form_id).load(url).hide();
+    width = $(form_id).width() + 30;
     $(form_id).dialog({
         autoOpen: false,
         modal: true,
         resizable: false,
         draggable: false,
-        width: $(form_id).find('form').width() + 30
+        width: width
     });
     $(form_id).dialog('open');
 }
