21d3d59712277d3b8b9b9d6626ec9a78244d9c7a
Justas Sadzevicius <justas@pov.lt>
2011-06-27 16:11:10 +0300

    Refactor some mislaid stuff.

--- src/schooltool/skin/flourish/resources/flourish.js
+++ src/schooltool/skin/flourish/resources/flourish.js
@@ -20,32 +20,6 @@
 */
 
 
-/* Stuff to refactor */
-
-$(document).ready(function() {
-    // accordion setup
-    $( ".person-info" ).accordion({
-        header: 'h2',
-        collapsible: true,
-        autoHeight: false,
-    });
-});
-
-
-function call_dialog(url, form_id) {
-    $(form_id).load(url).hide();
-    width = $(form_id).width() + 30;
-    $(form_id).dialog({
-        autoOpen: false,
-        modal: true,
-        resizable: false,
-        draggable: false,
-        width: width
-    });
-    $(form_id).dialog('open');
-}
-
-
 ST = new Object();
 
 /* ST local state extension */
