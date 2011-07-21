105db1ffe58c4547e848db2271c16448e174cc61
Douglas Cerna <douglascerna@yahoo.com>
2011-07-21 17:31:57 +0000

    - Added tertiary navigation manager for /courses view - Added manager and viewlet /courses view - Added managers and viewlets for course index view - Added /courses view - Added course views:   - Index   - Add   - Edit   - Delete dialog - Added flourish filter widget for course containers - Added flourish table formatter for course containers - Made resource add views to redirect to added object instead of container - Added missing schooltool i18n domain to some templates - Added css style for class used in two column display tables

--- src/schooltool/course/browser/course.py
+++ src/schooltool/course/browser/course.py
@@ -24,25 +24,54 @@ from zope.interface import implements
 from zope.component import adapts
 from zope.component import getMultiAdapter
 from zope.component import getUtility
+from zope.component import queryMultiAdapter
+from zope.container.interfaces import INameChooser
+from zope.interface import directlyProvides
 from zope.intid.interfaces import IIntIds
 from zope.app.form.browser.add import AddView
 from zope.publisher.interfaces.browser import IBrowserRequest
 from zope.publisher.browser import BrowserView
+from zope.security.checker import canAccess
+from zope.security import checkPermission
 from zope.security.proxy import removeSecurityProxy
 from zope.traversing.browser.interfaces import IAbsoluteURL
 from zope.traversing.browser.absoluteurl import absoluteURL
 from zope.i18n.interfaces.locales import ICollator
+from zope.i18n import translate
 from zope.viewlet.viewlet import ViewletBase
+from zc.table import table
+from zc.table.interfaces import ISortableColumn
+from z3c.form import field, button, form
+from z3c.form.interfaces import HIDDEN_MODE
 
+from schooltool.app.interfaces import ISchoolToolApplication
+from schooltool.common.inlinept import InheritTemplate
+from schooltool.common.inlinept import InlineViewPageTemplate
 from schooltool.term.interfaces import ITerm
 from schooltool.schoolyear.interfaces import ISchoolYear
+from schooltool.schoolyear.interfaces import ISchoolYearContainer
 from schooltool.skin.containers import ContainerView
 from schooltool.course.interfaces import ICourse, ICourseContainer
 from schooltool.course.interfaces import ILearner, IInstructor
-
-from schooltool.common import SchoolToolMessage as _
-
+from schooltool.course.course import Course
 from schooltool.skin.flourish.viewlet import Viewlet
+from schooltool.skin.flourish.containers import TableContainerView
+from schooltool.skin.flourish.containers import ContainerDeleteView
+from schooltool.skin.flourish.page import RefineLinksViewlet
+from schooltool.skin.flourish.page import LinkViewlet
+from schooltool.skin.flourish.page import Page
+from schooltool.skin.flourish.page import ModalFormLinkViewlet
+from schooltool.skin.flourish.form import Form
+from schooltool.skin.flourish.form import AddForm
+from schooltool.skin.flourish.form import DialogForm
+from schooltool.skin.flourish.form import DisplayForm
+from schooltool.skin.flourish.viewlet import ViewletManager
+from schooltool.table.table import url_cell_formatter
+from schooltool.table.table import SchoolToolTableFormatter
+from schooltool.table.table import FilterWidget
+from schooltool.table.interfaces import ITableFormatter
+from schooltool.table.table import LocaleAwareGetterColumn
+from schooltool.common import SchoolToolMessage as _
 
 
 class CourseContainerAbsoluteURLAdapter(BrowserView):
@@ -262,3 +291,342 @@ class FlourishCoursesViewlet(Viewlet):
                                      other['section_title'])
         return self.collator.cmp(this['course'], other['course'])
 
+
+class CoursesTertiaryNavigationManager(ViewletManager):
+
+    template = InlineViewPageTemplate("""
+        <ul tal:attributes="class view/list_class">
+          <li tal:repeat="item view/items"
+              tal:attributes="class item/class"
+              tal:content="structure item/viewlet">
+          </li>
+        </ul>
+    """)
+
+    list_class = 'third-nav'
+
+    @property
+    def items(self):
+        result = []
+        schoolyears = ISchoolYearContainer(self.context)
+        active = schoolyears.getActiveSchoolYear()
+        if 'schoolyear_id' in self.request:
+            schoolyear_id = self.request['schoolyear_id']
+            active = schoolyears.get(schoolyear_id, active)
+        for schoolyear in schoolyears.values():
+            url = '%s/%s?schoolyear_id=%s' % (
+                absoluteURL(self.context, self.request),
+                'courses',
+                schoolyear.__name__)
+            result.append({
+                    'class': schoolyear.first == active.first and 'active' or None,
+                    'viewlet': u'<a href="%s">%s</a>' % (url, schoolyear.title),
+                    })
+        return result
+
+
+class CoursesAddLinks(RefineLinksViewlet):
+    """Manager for Add links in CoursesView"""
+
+
+class CourseAddLinks(RefineLinksViewlet):
+    """Manager for Add links in CourseView"""
+
+    def render(self):
+        # This check is necessary because the user can be a leader
+        # of the context course, which gives him schooltool.edit on it
+        if canAccess(self.context.__parent__, '__delitem__'):
+            return super(CourseAddLinks, self).render()
+
+
+class CourseActionsLinks(RefineLinksViewlet):
+    """Manager for Action links in CourseView"""
+
+    def render(self):
+        # This check is necessary because the user can be a leader
+        # of the context course, which gives him schooltool.edit on it
+        if canAccess(self.context.__parent__, '__delitem__'):
+            return super(CourseActionsLinks, self).render()
+
+
+class CourseDeleteLink(ModalFormLinkViewlet):
+
+    @property
+    def dialog_title(self):
+        title = _(u'Delete ${course}',
+                  mapping={'course': self.context.title})
+        return translate(title, context=self.request)
+
+
+class CourseAddLinkViewlet(LinkViewlet):
+
+    @property
+    def schoolyear(self):
+        schoolyears = ISchoolYearContainer(self.context)
+        result = schoolyears.getActiveSchoolYear()
+        if 'schoolyear_id' in self.request:
+            schoolyear_id = self.request['schoolyear_id']
+            result = schoolyears.get(schoolyear_id, result)
+        return result
+
+    @property
+    def url(self):
+        courses = ICourseContainer(self.schoolyear)
+        return '%s/%s' % (absoluteURL(courses, self.request),
+                          'addSchoolToolCourse.html')
+
+class CourseAddLinkFromCourseViewlet(CourseAddLinkViewlet):
+
+    @property
+    def schoolyear(self):
+        return ISchoolYear(self.context.__parent__)
+
+    @property
+    def url(self):
+        courses = ICourseContainer(self.schoolyear)
+        return '%s/%s?camefrom=%s' % (
+            absoluteURL(courses, self.request),
+            'addSchoolToolCourse.html',
+            absoluteURL(self.context, self.request))
+
+
+class FlourishCoursesView(TableContainerView):
+
+    content_template = ViewPageTemplateFile('templates/f_courses.pt')
+
+    @property
+    def title(self):
+        schoolyear = self.schoolyear
+        return _('Courses for ${schoolyear}',
+                 mapping={'schoolyear': schoolyear.title})
+
+    @property
+    def schoolyear(self):
+        schoolyears = ISchoolYearContainer(self.context)
+        result = schoolyears.getActiveSchoolYear()
+        if 'schoolyear_id' in self.request:
+            schoolyear_id = self.request['schoolyear_id']
+            result = schoolyears.get(schoolyear_id, result)
+        return result
+
+    @property
+    def container(self):
+        schoolyear = self.schoolyear
+        return ICourseContainer(schoolyear)
+
+    def setUpTableFormatter(self, formatter):
+        formatter.setUp(formatters=[url_cell_formatter])
+
+    def update(self):
+        # XXX: maybe this lookup should be done in
+        #      flourish.containers.TableContainerView, instead of using context?
+        self.table = queryMultiAdapter((self.container, self.request), ITableFormatter)
+        super(FlourishCoursesView, self).update()
+
+
+class FlourishCourseContainerDeleteView(ContainerDeleteView):
+
+    def nextURL(self):
+        if 'CONFIRM' in self.request:
+            schoolyear = ISchoolYear(self.context)
+            url = '%s/%s?schoolyear_id=%s' % (
+                absoluteURL(ISchoolToolApplication(None), self.request),
+                'courses',
+                schoolyear.__name__)
+            return url
+        return ContainerDeleteView.nextURL(self)
+
+
+class FlourishCourseView(DisplayForm):
+
+    template = InheritTemplate(Page.template)
+    content_template = ViewPageTemplateFile('templates/f_course_view.pt')
+    fields = field.Fields(ICourse)
+    fields = fields.select('title', 'description', 'course_id', 'government_id', 'credits')
+
+    @property
+    def canModify(self):
+        return checkPermission('schooltool.edit', self.context)
+
+    @property
+    def title(self):
+        schoolyear = ISchoolYear(self.context.__parent__)
+        return _('Courses for ${schoolyear}',
+                 mapping={'schoolyear': schoolyear.title})
+
+    @property
+    def subtitle(self):
+        return self.context.title
+
+    def done_link(self):
+        schoolyear = ISchoolYear(self.context.__parent__)
+        url = '%s/%s?schoolyear_id=%s' % (
+            absoluteURL(ISchoolToolApplication(None), self.request),
+            'courses',
+            schoolyear.__name__)
+        return url
+
+    def updateWidgets(self):
+        super(FlourishCourseView, self).updateWidgets()
+        for widget in self.widgets.values():
+            if not widget.value:
+                widget.mode = HIDDEN_MODE
+
+    # XXX: leaders logic, duplicated from FlourishBaseResourceView
+
+    @property
+    def leaders_table(self):
+        return self.getTable(list(self.context.leaders))
+
+    def getTable(self, items):
+        persons = ISchoolToolApplication(None)['persons']
+        result = getMultiAdapter((persons, self.request), ITableFormatter)
+        result.setUp(table_formatter=table.StandaloneFullFormatter, items=items)
+        return result
+
+    def has_leaders(self):
+        return bool(list(self.context.leaders))
+
+
+class FlourishCourseAddView(AddForm):
+
+    template = InheritTemplate(Page.template)
+    label = None
+    legend = _('Course Information')
+    fields = field.Fields(ICourse)
+    fields = fields.select('title', 'description', 'course_id', 'government_id', 'credits')
+
+    def updateActions(self):
+        super(FlourishCourseAddView, self).updateActions()
+        self.actions['add'].addClass('button-ok')
+        self.actions['cancel'].addClass('button-cancel')
+
+    @button.buttonAndHandler(_('Add'))
+    def handleAdd(self, action):
+        super(FlourishCourseAddView, self).handleAdd.func(self, action)
+
+    @button.buttonAndHandler(_('Cancel'))
+    def handle_cancel_action(self, action):
+        if 'camefrom' in self.request:
+            url = self.request['camefrom']
+            self.request.response.redirect(url)
+            return
+        schoolyear = ISchoolYear(self.context)
+        url = '%s/%s?schoolyear_id=%s' % (
+            absoluteURL(ISchoolToolApplication(None), self.request),
+            'courses',
+            schoolyear.__name__)
+        self.request.response.redirect(url)
+
+    def create(self, data):
+        course = Course(data['title'], data.get('description'))
+        form.applyChanges(self, course, data)
+        return course
+
+    def add(self, course):
+        chooser = INameChooser(self.context)
+        name = chooser.chooseName(u'', course)
+        self.context[name] = course
+        self._course = course
+        return course
+
+    def nextURL(self):
+        return absoluteURL(self._course, self.request)
+
+    @property
+    def title(self):
+        schoolyear = ISchoolYear(self.context)
+        return _('Courses for ${schoolyear}',
+                 mapping={'schoolyear': schoolyear.title})
+
+
+class FlourishCourseEditView(Form, form.EditForm):
+
+    template = InheritTemplate(Page.template)
+    label = None
+    legend = _('Course Information')
+    fields = field.Fields(ICourse)
+    fields = fields.select('title', 'description', 'course_id', 'government_id', 'credits')
+
+    @property
+    def title(self):
+        return self.context.title
+
+    def update(self):
+        return form.EditForm.update(self)
+
+    @button.buttonAndHandler(_('Submit'), name='apply')
+    def handleApply(self, action):
+        super(FlourishCourseEditView, self).handleApply.func(self, action)
+        # XXX: hacky sucessful submit check
+        if (self.status == self.successMessage or
+            self.status == self.noChangesMessage):
+            url = absoluteURL(self.context, self.request)
+            self.request.response.redirect(url)
+
+    @button.buttonAndHandler(_("Cancel"))
+    def handle_cancel_action(self, action):
+        url = absoluteURL(self.context, self.request)
+        self.request.response.redirect(url)
+
+
+class FlourishCourseDeleteView(DialogForm, form.EditForm):
+    """View used for confirming deletion of a course."""
+
+    dialog_submit_actions = ('apply',)
+    dialog_close_actions = ('cancel',)
+    label = None
+
+    def updateDialog(self):
+        # XXX: fix the width of dialog content in css
+        if self.ajax_settings['dialog'] != 'close':
+            self.ajax_settings['dialog']['width'] = 544 + 16
+
+    @button.buttonAndHandler(_("Delete"), name='apply')
+    def handleDelete(self, action):
+        url = '%s/delete.html?delete.%s&CONFIRM' % (
+            absoluteURL(self.context.__parent__, self.request),
+            self.context.__name__)
+        self.request.response.redirect(url)
+        # We never have errors, so just close the dialog.
+        self.ajax_settings['dialog'] = 'close'
+
+    @button.buttonAndHandler(_("Cancel"))
+    def handle_cancel_action(self, action):
+        pass
+
+    def updateActions(self):
+        super(FlourishCourseDeleteView, self).updateActions()
+        self.actions['apply'].addClass('button-ok')
+        self.actions['cancel'].addClass('button-cancel')
+
+
+class FlourishCourseFilterWidget(FilterWidget):
+
+    template = ViewPageTemplateFile('templates/f_course_filter.pt')
+
+    def filter(self, results):
+        if 'SEARCH_TITLE' in self.request:
+            searchstr = self.request['SEARCH_TITLE'].lower()
+            results = [item for item in results
+                       if searchstr in item.title.lower() or
+                       (item.course_id and searchstr in item.course_id.lower())]
+        return results
+
+
+class FlourishCourseTableFormatter(SchoolToolTableFormatter):
+
+    def columns(self):
+        title = LocaleAwareGetterColumn(
+            name='title',
+            title=_(u'Title'),
+            getter=lambda i, f: i.title,
+            subsort=True)
+        course_id = LocaleAwareGetterColumn(
+            name='course_id',
+            title=_('Course ID'),
+            getter=lambda i, f: i.course_id or '',
+            subsort=True)
+        directlyProvides(title, ISortableColumn)
+        directlyProvides(course_id, ISortableColumn)
+        return [title, course_id]
