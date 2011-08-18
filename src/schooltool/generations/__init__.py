a992129d0e5c98258c24cae51d9b090454852a08
Justas Sadzevicius <justas@pov.lt>
2011-08-18 10:22:23 +0300

    Evolution to move levels to per-app container.

--- src/schooltool/generations/__init__.py
+++ src/schooltool/generations/__init__.py
@@ -25,6 +25,6 @@ $Id$
 from zope.app.generations.generations import SchemaManager
 
 schemaManager = SchemaManager(
-    minimum_generation=36,
-    generation=36,
+    minimum_generation=37,
+    generation=37,
     package_name='schooltool.generations')
