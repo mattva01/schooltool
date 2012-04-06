8915a0198f773f2c0ad118cd18dd2c287c785e16
Alan Elkner <aelkner@gmail.com>
2012-04-06 14:06:18 -0400

    added selenium test support to export package

---
+++ src/schooltool/export/stesting.py
@@ -0,0 +1,61 @@
+#
+# SchoolTool - common information systems platform for school administration
+# Copyright (c) 2012 Shuttleworth Foundation
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+#
+"""
+Selenium Functional Testing Utilities for export.
+"""
+
+import os
+
+from schooltool.testing.selenium import SeleniumLayer
+
+dir = os.path.abspath(os.path.dirname(__file__))
+filename = os.path.join(dir, 'stesting.zcml')
+
+export_selenium_layer = SeleniumLayer(filename,
+                                     __name__,
+                                     'export_selenium_layer')
+
+def registerSeleniumSetup():
+    try:
+        import selenium
+    except ImportError:
+        return
+    from schooltool.testing import registry
+    import schooltool.testing.selenium
+
+    def importXLS(browser, filename):
+        browser.query.link('School').click()
+        browser.query.link('Import XLS').click()
+        if filename:
+            import pkg_resources
+            elem = browser.query.name('xlsfile')
+            browser.query.name('xlsfile').file(
+                pkg_resources.resource_stream('schooltool.export.ftests', filename),
+                'application/excel',
+                filename)
+        page = browser.query.tag('html')
+        browser.query.button('Submit').click()
+        browser.wait(lambda: page.expired)
+
+    registry.register('SeleniumHelpers',
+        lambda: schooltool.testing.selenium.registerBrowserUI(
+            'import_xls', importXLS))
+
+registerSeleniumSetup()
+del registerSeleniumSetup
