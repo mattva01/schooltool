fc6976dfb46a20f8a6ffc61fd064786eb9fa11c0
Justas Sadzevicius <justas@pov.lt>
2012-08-09 19:42:42 +0300

    Flag to disable default url cell formatter for first cell

--- src/schooltool/table/ajax.py
+++ src/schooltool/table/ajax.py
@@ -68,6 +68,8 @@ class AJAXStandaloneSortFormatter(AJAXSortHeaderMixin,
 
 class Table(flourish.ajax.CompositeAJAXPart, TableContent):
 
+    no_default_url_cell_formatter = False
+
     container_wrapper = ViewPageTemplateFile('templates/f_ajax_table.pt')
 
     form_wrapper = InlineViewPageTemplate("""
@@ -97,7 +99,11 @@ class Table(flourish.ajax.CompositeAJAXPart, TableContent):
     def updateFormatter(self):
         if self._table_formatter is not None:
             return
-        self.setUp(formatters=[url_cell_formatter],
+        if self.no_default_url_cell_formatter:
+            formatters = []
+        else:
+            formatters = [url_cell_formatter]
+        self.setUp(formatters=formatters,
                    table_formatter=self.table_formatter,
                    batch_size=self.batch_size,
                    prefix=self.__name__,
