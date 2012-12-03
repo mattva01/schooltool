cea57541a99687b4d84a4253826c2231d070c9af
Justas Sadzevicius <justas@pov.lt>
2012-12-03 13:55:16 +0200

    Bugfix: allow relative paths for schooltool.flourish.template.*File

--- src/schooltool/skin/flourish/templates.py
+++ src/schooltool/skin/flourish/templates.py
@@ -27,11 +27,13 @@ from zope.browserpage.viewpagetemplatefile import ViewPageTemplateFile as File
 
 class XMLFile(File):
     def __init__(self, *args, **kw):
+        kw['_prefix'] = self.get_path_from_prefix(kw.get('prefix'))
         kw['content_type'] = 'text/xml'
         File.__init__(self, *args, **kw)
 
 
 class HTMLFile(File):
     def __init__(self, *args, **kw):
+        kw['_prefix'] = self.get_path_from_prefix(kw.get('prefix'))
         kw['content_type'] = 'text/html'
         File.__init__(self, *args, **kw)
